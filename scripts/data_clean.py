72686cf792947a6a36dba2a02d870229dd7ed5c3
ZYHowell <yhzhuang@cmu.ed>
2023-03-19 16:26:05 +0400

    fix code format

--- scripts/data_clean.py
+++ scripts/data_clean.py
@@ -1,5 +1,7 @@
+from html.parser import HTMLParser
 import json
 import logging
+import re
 
 import tqdm
 
@@ -11,7 +13,6 @@ OUTPUT_PATH = "./cleaned.json"
 
 def _get_html_tags(file_path: str):
     # Generate the to_handle_list. So long and cannot be handle one by one.
-    import re
     s = set()
     for l in open("file_path", "r"):
         for m in re.findall("</[^<>]+>", l):
@@ -20,8 +21,6 @@ def _get_html_tags(file_path: str):
 
 
 def _html_to_plain_text(val: str) -> str:
-    from html.parser import HTMLParser
-
     class HtmlFilter(HTMLParser):
         text = ""
 
@@ -40,16 +39,22 @@ def _html_to_rich_text(val: str) -> str:
     return h.handle(val)
 
 
+def _reformat_all_code(val: str) -> str:
+    # Input code format is:
+    # ```
+    # $<language>Copy code`$<exact_code_here>`
+    # ```
+    # This function convert it into the correct markdown format
+    match_pattern = re.compile("```\n" + "([^`]+)" + "Copy code`" + "([^`]+)" + "`\n```")
+    repl_format = r"```\g<1>\n\g<2>\n```"
+    return re.sub(match_pattern, repl_format, val)
+
+
 def _html_to_markdown(val: str) -> str:
     """can handle enum, table and code. Code not in the best format."""
     import markdownify
     out = markdownify.markdownify(val)
-    # TODO: the output format is:
-    # ```
-    # pythonCode`$<exact_code_here>`
-    # ```
-    # We need to convert it into the correct markdown format
-    return out
+    return _reformat_all_code(out)
 
 
 json_file = json.load(open(RAW_CONVERSATION_PATH, "r"))
