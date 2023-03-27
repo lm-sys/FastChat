67265a7a043a8d5ac49e5b4ef83c94e0c60896be
Lianmin Zheng <lianminzheng@gmail.com>
2023-03-27 14:23:49 +0000

    fix optional clean

--- chatserver/data/optional_clean.py
+++ chatserver/data/optional_clean.py
@@ -4,34 +4,37 @@ Example Usage: python3 -m chatserver.data.optional_clean --skip-lang en --reduce
 """
 import argparse
 import json
-from langdetect import detect
 import re
+
+import polyglot
+from polyglot.detect import Detector
+import pycld2
 from tqdm import tqdm
 
 
-def skip(conv, options: dict):
-    if options["lang"] != "all" or options["skip_lang"] != "none":
-        cnt = 0
-        for sentence in conv["conversations"][:10]:
-            try:
-                lang = detect(sentence["value"])
-                if options["lang"] != "all" and  lang != options["lang"]:
-                    cnt += 1
-                if lang == options["skip_lang"]:
-                    return True
-                if options["skip_lang"] == "ko" and "\ubc88" in sentence["value"]:
-                    return True
-            except:
-                cnt += 1
-                pass
-        if cnt > min(5, len(conv["conversations"]) // 2):
+def skip(conv, args):
+    # Remove certain languages
+    if args.lang != "all" or args.skip_lang is not None:
+        text = "\n".join([x["value"] for x in conv["conversations"]])
+        try:
+            lang_code = Detector(text).language.code
+        except (pycld2.error, polyglot.detect.base.UnknownLanguage):
+            lang_code = "unknown"
+
+        if args.lang != "all" and lang_code != args.lang:
             return True
-    if options["reduce_rep"]:
+
+        if lang_code == args.skip_lang:
+            return True
+
+    # Remove repetitive numbers
+    if args.reduce_rep:
         for sentence in conv["conversations"]:
             val = sentence["value"]
             sub = re.search(r"(\d)\1{8}", val)
             if sub is not None:
                 return True
+
     return False
  
 
@@ -39,11 +42,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--in-file", type=str, required=True)
     parser.add_argument("--out-file", type=str, default="")
-    # NOTICE This will also remove data that use two languages.
     parser.add_argument("--lang", type=str, default="all",
                         choices=["all", "en"])
-    parser.add_argument("--skip-lang", type=str, default="none")
-    # DANGER Be careful about turn on the reduce_rep, which may remove some good data.
+    parser.add_argument("--skip-lang", type=str)
+    # NOTE: Be careful about reduce_rep which may remove some good data.
     # For example, addresses could have long consecutive 0's
     parser.add_argument("--reduce-rep", action="store_true")
     args = parser.parse_args()
@@ -53,13 +55,13 @@ if __name__ == "__main__":
     lang = args.lang
     skip_lang = args.skip_lang
     reduce_rep = args.reduce_rep
-    assert (lang == "all" or skip_lang == "none")
+    assert (lang == "all" or skip_lang is None)
 
     if out_file == "":
         out_file = "sharegpt_clean"
         if lang != "all":
             out_file += "_" + lang
-        if skip_lang != "none":
+        if skip_lang is not None:
             out_file += "_skip_" + skip_lang
         if reduce_rep:
             out_file += "_reduce_rep"
@@ -70,9 +72,8 @@ if __name__ == "__main__":
 
     new_content = []
     for conv in tqdm(content):
-        if not skip(conv, vars(args)):
+        if not skip(conv, args):
             new_content.append(conv)
 
     print(f"return {len(new_content)} out of {len(content)}, start dump ...")
     json.dump(new_content, open(out_file, "w"), indent=2)
-
--- docs/commands.md
+++ docs/commands.md
@@ -18,7 +18,8 @@ python3 -m chatserver.serve.gradio_web_server
 ### Data cleanning
 ```
 python3 -m chatserver.data.clean_sharegpt --in sharegpt_20230322_html.json --out sharegpt_20230322_clean.json
-python3 -m chatserver.data.split_long_conversation --in sharegpt_20230322_clean.json --out sharegpt_20230322_split.json --model-name /home/ubuntu/model_weights/hf-llama-7b/
+python3 -m chatserver.data.optional_clean --in sharegpt_20230322_clean.json --out sharegpt_20230322_clean_lang.json --skip-lang
+python3 -m chatserver.data.split_long_conversation --in sharegpt_20230322_clean_lang.json --out sharegpt_20230322_clean_lang_split.json --model-name /home/ubuntu/model_weights/hf-llama-7b/
 
 gsutil cp sharegpt_20230322_clean.json gs://model-weights/sharegpt/
 gsutil cp sharegpt_20230322_split.json gs://model-weights/sharegpt/
