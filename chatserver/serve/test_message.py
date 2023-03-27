78c260ee2427780dddb817559a2b8ba23b175b0f
Lianmin Zheng <lianminzheng@gmail.com>
2023-03-27 11:00:40 +0000

    Update test message

--- chatserver/serve/test_message.py
+++ chatserver/serve/test_message.py
@@ -26,7 +26,7 @@ def main():
         return
 
     conv = default_conversation.copy()
-    conv.append_message(conv.roles[0], "Tell me a long story with more than 1000 words.")
+    conv.append_message(conv.roles[0], args.message)
     prompt = conv.get_prompt()
 
     headers = {"User-Agent": "ChatServer Client"}
@@ -55,6 +55,8 @@ if __name__ == "__main__":
     parser.add_argument("--worker-address", type=str)
     parser.add_argument("--model-name", type=str, default="facebook/opt-350m")
     parser.add_argument("--max-new-tokens", type=int, default=32)
+    parser.add_argument("--message", type=str, default=
+        "Tell me a story with more than 1000 words.")
     args = parser.parse_args()
 
     main()
