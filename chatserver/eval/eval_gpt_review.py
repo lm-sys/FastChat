65fe047c22eb144e8508b484bdf323b402a79ed9
Wei-Lin Chiang <infwinston@gmail.com>
2023-03-29 16:43:48 -0700

    update

--- chatserver/eval/eval_gpt_review.py
+++ chatserver/eval/eval_gpt_review.py
@@ -5,31 +5,41 @@ import os
 import openai
 import tqdm
 import ray
+import time
 
-@ray.remote
+@ray.remote(num_cpus=4)
 def get_eval(content: str, max_tokens: int):
-    response = openai.ChatCompletion.create(
-        model='gpt-4',
-        messages=[{
-            'role': 'system',
-            'content': 'You are a helpful and precise assistant for checking the quality of the answer.'
-        }, {
-            'role': 'user',
-            'content': content,
-        }],
-        temperature=0.2,  # TODO: figure out which temperature is best for evaluation
-        max_tokens=max_tokens,
-    )
+    try:
+        response = openai.ChatCompletion.create(
+            model='gpt-4',
+            messages=[{
+                'role': 'system',
+                'content': 'You are a helpful and precise assistant for checking the quality of the answer.'
+            }, {
+                'role': 'user',
+                'content': content,
+            }],
+            temperature=0.2,  # TODO: figure out which temperature is best for evaluation
+            max_tokens=max_tokens,
+        )
+    except Exception as e:
+        print(e)
+        return 'error'
     return response['choices'][0]['message']['content']
 
 
 def parse_score(review):
-    score_pair = review.split('\n')[0]
-    score_pair = score_pair.replace(',', ' ')
-    sp = score_pair.split(' ')
-    if len(sp) == 2:
-        return [float(sp[0]), float(sp[1])]
-    else:
+    try:
+        score_pair = review.split('\n')[0]
+        score_pair = score_pair.replace(',', ' ')
+        sp = score_pair.split(' ')
+        if len(sp) == 2:
+            return [float(sp[0]), float(sp[1])]
+        else:
+            print('error', review)
+            return [-1, -1]
+    except Exception as e:
+        print(e)
         print('error', review)
         return [-1, -1]
 
@@ -53,14 +63,18 @@ if __name__ == '__main__':
 
     review_file = open(f'{args.output}', 'w')
 
+    js_list = []
     handles = []
-    idx = 1
+    idx = 0
     for ques_js, ans1_js, ans2_js in zip(f_q, f_ans1, f_ans2):
+        # if idx == 10:
+        #     break
+
         ques = json.loads(ques_js)['question']
         ans1 = json.loads(ans1_js)['answer']
         ans2 = json.loads(ans2_js)['answer']
 
-        category = json.loads(ans2_js)['category']
+        category = json.loads(ques_js)['category']
         if category in rule_dict:
             rule = rule_dict[category]
         else:
@@ -71,17 +85,21 @@ if __name__ == '__main__':
                    f'[{role} 1]\n{ans1}\n\n[End of {role} 1]\n\n'
                    f'[{role} 2]\n{ans2}\n\n[End of {role} 2]\n\n'
                    f'[System]\n{prompt}\n\n')
+        js_list.append({
+            'id': idx+1,
+            'question': ques,
+            'answer1': ans1,
+            'answer2': ans2,
+            'category': category})
+        idx += 1
         handles.append(get_eval.remote(content, args.max_tokens))
+        # To avoid the rate limit set by OpenAI
+        time.sleep(10)
 
     reviews = ray.get(handles)
-    for review in reviews:
+    for idx, review in enumerate(reviews):
         scores = parse_score(review)
-        js = {'content': review,
-              'id': idx,
-              'question': ques,
-              'answer1': ans1,
-              'answer2': ans2,
-              'category': category,
-              'tuple': scores}
-        review_file.write(json.dumps(js) + '\n')
+        js_list[idx]['content'] = review
+        js_list[idx]['tuple'] = scores
+        review_file.write(json.dumps(js_list[idx]) + '\n')
     review_file.close()
