4d8df4891f9d45b6af2f07713369b70e565ad496
Lianmin Zheng <lianminzheng@gmail.com>
2023-03-30 07:02:06 +0000

    fix multiline input

--- fastchat/utils.py
+++ fastchat/utils.py
@@ -103,11 +103,14 @@ def violates_moderation(text):
     url = "https://api.openai.com/v1/moderations"
     headers = {"Content-Type": "application/json",
                "Authorization": "Bearer " + os.environ["OPENAI_API_KEY"]}
+    text = text.replace("\n", "")
     data = "{" + '"input": ' + f'"{text}"' + "}"
     try:
         ret = requests.post(url, headers=headers, data=data, timeout=5)
         flagged = ret.json()["results"][0]["flagged"]
     except requests.exceptions.RequestException as e:
+        flagged = False
+    except KeyError as e:
         flagged = False
 
     return flagged
