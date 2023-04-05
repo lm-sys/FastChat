07cdd410cefea3d863232c6c3a879f3aac9eea9f
Lianmin Zheng <lianminzheng@gmail.com>
2023-04-05 11:19:35 -0700

    Apply suggestions from code review

--- fastchat/train/train.py
+++ fastchat/train/train.py
@@ -187,8 +187,8 @@ def preprocess(
     for target, source in zip(targets, sources):
         tokenized_sentence = _tokenize_fn([s["value"] for s in source], tokenizer)
         tokenized_lens = tokenized_sentence["input_ids_lens"]
-        # empirical perfect fix. Current ### is tokenized into 2 in conv, 1 in
-        # single sentence and thus avoids this fix
+        # Currently, "###" is tokenized into 2 tokens in the whole conversation,
+        # and 1 token in a single sentence, so we do not need to use the code below.
         # tokenized_lens = [l-1 for l in tokenized_lens]
         speakers = [sentence["from"] for sentence in source]
         ids = tokenized_sentence["input_ids"]
