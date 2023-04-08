5525357e95d7ebf0ef88a893c601656c149d4368
Siyuan <suquark@gmail.com>
2023-04-08 15:55:18 -0700

    update

--- fastchat/serve/cli.py
+++ fastchat/serve/cli.py
@@ -5,6 +5,16 @@ Usage:
 python3 -m fastchat.serve.cli --model ~/model_weights/llama-7b
 """
 import argparse
+import re
+
+from prompt_toolkit import prompt
+from prompt_toolkit import PromptSession
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
+from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.history import InMemoryHistory
+from rich.console import Console
+from rich.markdown import Markdown
+
 from fastchat.serve.inference import chat_loop, ChatIO
 
 
@@ -24,19 +34,26 @@ class SimpleChatIO(ChatIO):
 
 class MarkdownChatIO(ChatIO):
     def __init__(self):
-        pass
+        self._prompt_session = PromptSession(history=InMemoryHistory())
+        self._completer = WordCompleter(words=['!exit', '!reset'], pattern=re.compile('$'))
+        self._console = Console()
 
     def prompt_for_input(self, role) -> str:
-        return input(f"{role}: ")
+        self._console.print(f"{role}: ", end="")
+        return self._prompt_session.prompt(
+            completer=self._completer,
+            multiline=True,
+            auto_suggest=AutoSuggestFromHistory(),
+            key_bindings=None)
 
     def prompt_for_output(self, role: str):
-        print(f"{role}: ", end="", flush=True)
+        self._console.print(f"{role}: ", end="")
 
     def append_output(self, output: str):
-        print(output, end=" ", flush=True)
+        self._console.print(output, end="")
 
     def finalize_output(self, output: str):
-        print(output, flush=True)
+        self._console.print(output)
 
 
 def main(args):
