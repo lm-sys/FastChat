f4ba7c130ffcad54c9433ff7824d60ee71ad1f4d
andy-yang-1 <andy_yang@sjtu.edu.cn>
2023-04-14 11:10:01 +0800

    reduce peak memory to 8GB with adaptive loading

--- fastchat/model/apply_delta.py
+++ fastchat/model/apply_delta.py
@@ -2,7 +2,7 @@
 Apply the delta weights on top of a base model.
 
 Usage:
-python3 -m fastchat.model.apply_delta --base ~/model_weights/llama-13b --target ~/model_weights/vicuna-13b --delta lmsys/vicuna-13b-delta-v1
+python3 -m fastchat.model.apply_delta --base ~/model_weights/llama-13b --target ~/model_weights/vicuna-13b-v1.1 --delta lmsys/vicuna-13b-delta
 """
 import argparse
 
@@ -13,54 +13,96 @@ from transformers import LlamaTokenizer, AutoConfig
 import gc
 from torch import nn
 import shutil
+import os, glob
+
+def process_weight(base_model_path,tmp_path="./tmp/"):
+
+
+    if not os.path.exists(tmp_path):
+        os.makedirs(tmp_path)
+
+
+    split_size = 4 * 1024 * 1024 * 1024
+
+    file_pattern = os.path.join(base_model_path, "pytorch_model-*.bin")
+    files = glob.glob(file_pattern)
+
+    part = 0
+
+    for file_path in files:
+        state_dict = torch.load(file_path, map_location=torch.device("cpu"))
+        new_state_dict = {}
+
+        current_size = 0
+
+        for name, param in state_dict.items():
+            param_size = param.numel() * param.element_size()
+
+            if current_size + param_size > split_size:
+                new_file_name = f"part{part}.bin"
+                new_file_path = os.path.join(tmp_path, new_file_name)
+                torch.save(new_state_dict, new_file_path)
+                print(f"Saved {new_file_path}")
+                current_size = 0
+                new_state_dict = None
+                gc.collect()
+                new_state_dict = {}
+                part += 1
+
+            new_state_dict[name] = param
+            current_size += param_size
+
+        new_file_name = f"part{part}.bin"
+        new_file_path = os.path.join(tmp_path, new_file_name)
+        torch.save(new_state_dict, new_file_path)
+        print(f"Saved {new_file_path}")
+        current_size = 0
+        new_state_dict = None
+        gc.collect()
+        new_state_dict = {}
+        part += 1
 
 def apply_delta(base_model_path, target_model_path, delta_path):
 
     print(f"Loading the delta from {delta_path}")
-    # delta_tokenizer = AutoTokenizer.from_pretrained(delta_path, use_fast=False)
-
 
     DEFAULT_PAD_TOKEN = "[PAD]"
     base_tokenizer = AutoTokenizer.from_pretrained(base_model_path, use_fast=False)
     num_new_tokens = base_tokenizer.add_special_tokens(dict(pad_token=DEFAULT_PAD_TOKEN))
     print("num_new_tokens: ",num_new_tokens)
 
-    delta_num = 0
-    delta_state_dict = torch.load(f"{delta_path}/pytorch_model-0000{str(delta_num%3+1)}-of-00003.bin", map_location=torch.device('cpu'))
     config = AutoConfig.from_pretrained(base_model_path)
 
+    tmp_llama_path = "./tmp1/"
+    tmp_delta_path = "./tmp2/"
     
+    process_weight(base_model_path,tmp_llama_path)
+    process_weight(delta_path,tmp_delta_path)
+
+    llama_pattern = os.path.join(tmp_llama_path, "part*.bin")
+    llama_files = glob.glob(llama_pattern)
+    delta_pattern = os.path.join(tmp_delta_path, "part*.bin")
+    delta_files = glob.glob(delta_pattern)
+    delta_state_dict = torch.load(delta_files[0], map_location=torch.device('cpu'))
+
 
     print("Applying the delta")
-    for i in range(42):
-        if i < 10:
-            file_name = f"pytorch_model-0000{str(i)}-of-00041.bin"
-        else:
-            file_name = f"pytorch_model-000{str(i)}-of-00041.bin"
-        state_dict_path = f"{base_model_path}/{file_name}"
-
-        state_dict = torch.load(state_dict_path, map_location=torch.device('cpu'))
-
+    for llama_file in llama_files:
+        state_dict = torch.load(llama_file, map_location=torch.device("cpu"))
         for name, param in tqdm(state_dict.items(), desc="Applying delta"):
-
-            while name not in delta_state_dict:
-                delta_num = (delta_num + 1) % 3
-                delta_state_dict = torch.load(f"{delta_path}/pytorch_model-0000{str(delta_num%3+1)}-of-00003.bin", map_location=torch.device('cpu'))
-                gc.collect()
-
-            if param.shape != delta_state_dict[name].shape:
-                new_embeddings = torch.zeros(len(base_tokenizer),state_dict[name].shape[1])
-                new_embeddings[:len(base_tokenizer)-num_new_tokens] = state_dict[name]
-                state_dict[name] = new_embeddings
+            if name not in delta_state_dict:
+                for delta_file in delta_files:
+                    delta_state_dict = torch.load(delta_file, map_location=torch.device('cpu'))
+                    gc.collect()
+                    if name in delta_state_dict:
+                        break
+        if param.shape != delta_state_dict[name].shape:
+            new_embeddings = torch.zeros(len(base_tokenizer),state_dict[name].shape[1])
+            new_embeddings[:len(base_tokenizer)-num_new_tokens] = state_dict[name]
+            state_dict[name] = new_embeddings
                 
-            state_dict[name] += delta_state_dict[name]
-
-        # save state_dict
-        torch.save(state_dict, f"{target_model_path}/{file_name}")
-        state_dict = None 
+        state_dict[name] += delta_state_dict[name]
         gc.collect()
-        del state_dict
-
 
     print(f"Saving the target model to {target_model_path}")
     base_tokenizer.save_pretrained(target_model_path)
