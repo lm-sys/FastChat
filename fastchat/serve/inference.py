3df4c459aab9cdd56f2a454537de5a5441a940e7
HT Liu <shomyliu@gmail.com>
2023-04-16 18:31:49 +0800

    Remove attention mask in inference.py to fit different models (#375)
    Co-authored-by: liuhongtao01_dxm <liuhongtao01@duxiaoman.com>

--- fastchat/serve/inference.py
+++ fastchat/serve/inference.py
@@ -86,11 +86,8 @@ def generate_stream(model, tokenizer, params, device,
             logits = out.logits
             past_key_values = out.past_key_values
         else:
-            attention_mask = torch.ones(
-                1, past_key_values[0][0].shape[-2] + 1, device=device)
             out = model(input_ids=torch.as_tensor([[token]], device=device),
                         use_cache=True,
-                        attention_mask=attention_mask,
                         past_key_values=past_key_values)
             logits = out.logits
             past_key_values = out.past_key_values
