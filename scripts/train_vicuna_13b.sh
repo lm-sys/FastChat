bc51b4c1116c2fdcfaee74191fd58409b9537203
Ying Sheng <sqy1415@gmail.com>
2023-04-24 00:52:13 +0000

    Use cpu offload

--- scripts/train_vicuna_13b.sh
+++ scripts/train_vicuna_13b.sh
@@ -4,9 +4,9 @@ torchrun --nproc_per_node=8 --master_port=20001 fastchat/train/train_mem.py \
     --bf16 True \
     --output_dir output_13b \
     --num_train_epochs 3 \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 16 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "steps" \
     --eval_steps 1200 \
     --save_strategy "steps" \
@@ -16,7 +16,7 @@ torchrun --nproc_per_node=8 --master_port=20001 fastchat/train/train_mem.py \
     --warmup_ratio 0.04 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --fsdp "full_shard auto_wrap" \
+    --fsdp "full_shard auto_wrap offload" \
     --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
     --tf32 True \
     --model_max_length 2048 \
