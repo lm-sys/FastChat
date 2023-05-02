038d466cfb457f237325d2d6b523bdf2e7423a62
andy-yang-1 <andy_yang@sjtu.edu.cn>
2023-05-02 09:55:55 +0800

    fix layer

--- fastchat/serve/model_worker.py
+++ fastchat/serve/model_worker.py
@@ -212,7 +212,7 @@ class ModelWorker:
         tokenizer = self.tokenizer
         input_ids = tokenizer.encode(params["input"], return_tensors='pt').to(self.device)
         model_output = self.model(input_ids, output_hidden_states=True)
-        data = model_output.hidden_states[0][0]
+        data = model_output.hidden_states[-1][0]
         return data.tolist()
 
 
