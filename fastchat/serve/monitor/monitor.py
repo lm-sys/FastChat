f2b5e335fdf8c94ab66a5157d6977ae721752a71
Lianmin Zheng <lianminzheng@gmail.com>
2023-05-03 17:02:33 +0000

    Update blog link

--- fastchat/serve/monitor/monitor.py
+++ fastchat/serve/monitor/monitor.py
@@ -91,7 +91,7 @@ def build_leaderboard_tab(elo_results_file):
     md_1 = gr.Markdown(md)
     gr.Markdown('''## More Statistics\n
 Here, we have added some additional figures to show more statistics. The code for generating them is also included in this [notebook](https://colab.research.google.com/drive/1lAQ9cKVErXI1rEYq7hTKNaCQ5Q8TzrI5?usp=sharing).
-Please note that you may see different orders from different ranking methods. This is expected for models that perform similarly, as demonstrated by the confidence interval in the bootstrap figure. Going forward, we will use the classical Elo calculation because of its scalability and interpretability. You can find more discussions in this blog [post](https://lmsys.org/blog/2023-05-03-arena/).
+Please note that you may see different orders from different ranking methods. This is expected for models that perform similarly, as demonstrated by the confidence interval in the bootstrap figure. Going forward, we prefer the classical Elo calculation because of its scalability and interpretability. You can find more discussions in this blog [post](https://lmsys.org/blog/2023-05-03-arena/).
 ''')
     
     with gr.Row():
