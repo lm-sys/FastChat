cd56f815a25ad54208befb8835fad1b9efa3b993
Lianmin Zheng <lianminzheng@gmail.com>
2023-05-12 08:25:40 +0000

    improve docs

--- tests/test_openai_sdk.py
+++ tests/test_openai_sdk.py
@@ -6,9 +6,20 @@ openai.api_base = "http://localhost:8000/v1"
 model = "vicuna-7b-v1.1"
 
 
+def test_list_models():
+    model_list = openai.Model.list()
+    print(model_list["data"][0]["id"])
+
+
 def test_completion():
-    completion = openai.Completion.create(model=model, prompt="Once upon a time", max_tokens=64)
-    print(completion.choices[0].text)
+    prompt = "Once upon a time"
+    completion = openai.Completion.create(model=model, prompt=prompt, max_tokens=64)
+    print(prompt + completion.choices[0].text)
+
+
+def test_embedding():
+    embedding = openai.Embedding.create(model=model, input="Hello world!")
+    print(embedding["usage"])
 
 
 def test_chat_completion():
@@ -25,9 +36,12 @@ def test_chat_completion_stream():
     for chunk in res:
         content = chunk["choices"][0]["delta"].get("content", "")
         print(content, end="", flush=True)
+    print()
 
 
 if __name__ == "__main__":
+    test_list_models()
     test_completion()
+    test_embedding()
     test_chat_completion()
     test_chat_completion_stream()
