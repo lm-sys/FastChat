ff7d3f6a1537973640cefad19f2f0d76f4e52c75
zhisbug <zhisbug@gmail.com>
2023-05-15 02:54:09 -0700

    update

--- fastchat/train/train_xformer.py
+++ fastchat/train/train_xformer.py
@@ -1,10 +1,12 @@
 # Make it more memory efficient by monkey patching the LLaMA model with XFormer's
 # memory-efficient attention.
+# Use this one instead train_mem.py if you are training on V100.
 
 # Need to call this before importing transformers.
 from fastchat.train.llama_xformer_monkey_patch import (
     replace_llama_attn_with_xformer
 )
+
 
 if __name__ == "__main__":
     replace_llama_attn_with_xformer()
