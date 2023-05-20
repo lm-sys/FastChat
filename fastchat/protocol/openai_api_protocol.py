b1da1f34e9d4b119105e5d1faed4c331349cafa4
Kalila <kalila@neocadia.com>
2023-05-20 18:44:10 +0000

    Fix bug in protocol spec.

--- fastchat/protocol/openai_api_protocol.py
+++ fastchat/protocol/openai_api_protocol.py
@@ -50,7 +50,7 @@ class UsageInfo(BaseModel):
 
 class ChatCompletionRequest(BaseModel):
     model: str
-    messages: List[Dict[str, str]]
+    messages: Union[str, List[Dict[str, str]]]
     temperature: Optional[float] = 0.7
     top_p: Optional[float] = 1.0
     n: Optional[int] = 1
