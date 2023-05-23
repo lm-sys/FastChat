65c37f770a7e4ad00022419499cd50c2591b6719
Kevin Canwen Xu <canwenxu@126.com>
2023-05-23 16:15:05 -0700

    Update model_registry.py

--- fastchat/model/model_registry.py
+++ fastchat/model/model_registry.py
@@ -137,5 +137,5 @@ register_model_info(
     ["baize-v2-7b", "baize-v2-13b"],
     "Baize v2",
     "https://github.com/project-baize/baize-chatbot",
-    "A chatbot fine-tuned from LLaMA with ChatGPT self-data and Self-Disillation with Feedback (SDF) by UCSD and SYSU.",
+    "A chatbot fine-tuned from LLaMA with ChatGPT self-chat data and Self-Disillation with Feedback (SDF) by UCSD and SYSU.",
 )
