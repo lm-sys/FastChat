e8913be25985fe20ecf11378ec6ea98b5154a357
BabyChouSr <emu4yolife@gmail.com>
2023-05-25 16:55:26 -0700

    Revert "add warning for flash attention"
    This reverts commit f4d7b2cef7c854a3ea5ec6ce347097bc57f0d570.

--- fastchat/train/llama_flash_attn_monkey_patch.py
+++ fastchat/train/llama_flash_attn_monkey_patch.py
@@ -1,5 +1,4 @@
 from typing import List, Optional, Tuple
-import logging
 
 import torch
 from torch import nn
@@ -109,12 +108,6 @@ def _prepare_decoder_attention_mask(
 
 
 def replace_llama_attn_with_flash_attn():
-    cuda_major, cuda_minor = torch.cuda.get_device_capability()
-    if (cuda_major, cuda_minor) != (8, 0) or  (cuda_major, cuda_minor) != (9, 0):
-        logging.warning(
-            "Flash attention is only supported on A100 or H100 GPU during training due to head dim > 64 backward."
-            "ref: https://github.com/HazyResearch/flash-attention/issues/190#issuecomment-1523359593"
-        )
     transformers.models.llama.modeling_llama.LlamaModel._prepare_decoder_attention_mask = (
         _prepare_decoder_attention_mask
     )
