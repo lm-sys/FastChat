4b4fed6a9b9cde317d0532cc8ea8b3ebcfec1fd6
lihui <yezhimeih7@dingtalk.com>
2023-06-07 20:00:19 +0800

    fix chatglm-6b:when use /v1/completions ,IndexError: string index out of range

--- fastchat/model/chatglm_model.py
+++ fastchat/model/chatglm_model.py
@@ -40,11 +40,11 @@ def chatglm_generate_stream(
 
     hist = []
     query = ""
-    if type(messages) == list:
+    if type(messages) is list:
         for i in range(0, len(messages) - 2, 2):
             hist.append((messages[i][1], messages[i + 1][1]))
-            query = messages[-2][1]
-    elif type(messages) == str:
+        query = messages[-2][1]
+    elif type(messages) is str:
         query = messages
 
     input_echo_len = stream_chat_token_num(tokenizer, query, hist)
