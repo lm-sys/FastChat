99eba7e433e481875d8e27dba4014c4990ccba3e
Lianmin Zheng <lianminzheng@gmail.com>
2023-06-09 20:24:49 +0000

    Print worker id

--- fastchat/serve/cacheflow_worker.py
+++ fastchat/serve/cacheflow_worker.py
@@ -127,7 +127,8 @@ class CacheFlowWorker:
         logger.info(
             f"Send heart beat. Models: {[self.model_name]}. "
             f"Semaphore: {pretty_print_semaphore(model_semaphore)}. "
-            f"global_counter: {global_counter}"
+            f"global_counter: {global_counter}. "
+            f"worker_id: {worker_id}. "
         )
 
         url = self.controller_addr + "/receive_heart_beat"
