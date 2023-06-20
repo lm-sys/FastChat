f4468f355977b538bc49bac05837363f6683f3d5
Trangle <kw_w@foxmail.com>
2023-06-20 20:05:43 +0800

    update robin template
    I test this worked for me, which avoid unstoppable output

--- fastchat/conversation.py
+++ fastchat/conversation.py
@@ -120,12 +120,12 @@ class Conversation:
                     ret += role + ": " + "<s>"
             return ret
         elif self.sep_style == SeparatorStyle.ROBIN:
-            ret = self.system
+            ret = self.system + self.sep
             for role, message in self.messages:
                 if message:
-                    ret += role + ":" + message + self.sep
+                    ret += role + ":\n" + message + self.sep
                 else:
-                    ret += role + ":"
+                    ret += role + ":\n"
             return ret
         else:
             raise ValueError(f"Invalid style: {self.sep_style}")
@@ -523,7 +523,7 @@ register_conv_template(
         messages=(),
         offset=0,
         sep_style=SeparatorStyle.ROBIN,
-        sep="</s>",
+        sep="\n",
         stop_token_ids=[2, 396],
         stop_str="###",
     )
