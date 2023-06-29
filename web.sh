105d470aa9c9d347b22be8dd58358c31d3ca1b7c
Alexandre Strube <surak@surak.eti.br>
2023-06-29 12:17:43 +0000

    Fixes

--- web.sh
+++ web.sh
@@ -3,6 +3,10 @@
 cd /p/haicluster/llama/FastChat
 source sc_venv_template/activate.sh
 
-#python3 -m fastchat.serve.gradio_web_server_multi --host 0.0.0.0
+#python3 -m fastchat.serve.gradio_web_server_multi --share
 #python3 -m fastchat.serve.gradio_web_server --model-list-mode=reload --host 0.0.0.0
-python3 fastchat/serve/gradio_web_server_branded.py --model-list-mode=reload --host 0.0.0.0
+python3 fastchat/serve/gradio_web_server_branded.py \
+        --share \
+        --model-list-mode=reload \
+        --host 0.0.0.0 \
+
