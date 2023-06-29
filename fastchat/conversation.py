ddf6a33350ac7a1a10fd29208a6e72a2cdcc3a8e
Fazlul Shahriar <fshahriar@gmail.com>
2023-06-29 02:57:33 -0400

    Fix undefined variables found by linter (#1800)

--- fastchat/conversation.py
+++ fastchat/conversation.py
@@ -223,7 +223,9 @@ conv_templates: Dict[str, Conversation] = {}
 def register_conv_template(template: Conversation, override: bool = False):
     """Register a new conversation template."""
     if not override:
-        assert template.name not in conv_templates, f"{name} has been registered."
+        assert (
+            template.name not in conv_templates
+        ), f"{template.name} has been registered."
     conv_templates[template.name] = template
 
 
