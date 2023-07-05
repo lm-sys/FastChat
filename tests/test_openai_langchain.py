62e09d39355d0bef950f79d6fd2286b4b38bd4d8
Lianmin Zheng <lianminzheng@gmail.com>
2023-07-05 07:48:53 +0000

    update

--- tests/test_openai_langchain.py
+++ tests/test_openai_langchain.py
@@ -1,7 +1,10 @@
+# Usage:
 # python3 -m fastchat.serve.model_worker --model-path lmsys/vicuna-7b-v1.3 --model-names gpt-3.5-turbo,text-davinci-003,text-embedding-ada-002
 # export OPENAI_API_BASE=http://localhost:8000/v1
 # export OPENAI_API_KEY=EMPTY
 # wget https://raw.githubusercontent.com/hwchase17/langchain/v0.0.200/docs/modules/state_of_the_union.txt
+
+import os
 
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import TextLoader
@@ -31,4 +34,6 @@ def test_chain():
 
 
 if __name__ == "__main__":
+    os.environ["OPENAI_API_BASE"] = "http://localhost:8000/v1"
+    os.environ["OPENAI_API_KEY"] = "empty"
     test_chain()
