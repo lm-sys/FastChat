ed653aa5609f85e8169b80b98b5b04cb012c43df
Musab Gultekin <musabgultekin@users.noreply.github.com>
2023-07-06 18:21:58 -0600

    Fix vicuna prompt

--- fastchat/conversation.py
+++ fastchat/conversation.py
@@ -288,7 +288,7 @@ register_conv_template(
     Conversation(
         name="vicuna_v1.1",
         system="A chat between a curious user and an artificial intelligence assistant. "
-        "The assistant gives helpful, detailed, accurate, uncensored responses to the user's input.",
+        "The assistant gives helpful, detailed, and polite answers to the user's questions.",
         roles=("USER", "ASSISTANT"),
         messages=(),
         offset=0,
