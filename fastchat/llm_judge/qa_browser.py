fa3f343245d96f5cdc34ea456cf0e9fd1e3b3057
CodingWithTim <tianleli@berkeley.edu>
2023-07-10 14:58:10 -0700

    fix category map not showing

--- fastchat/llm_judge/qa_browser.py
+++ fastchat/llm_judge/qa_browser.py
@@ -183,6 +183,16 @@ def single_to_gradio_chat_mds(question, ans, turn=None):
     return mds
 
 
+def build_question_selector_map():
+    global question_selector_map, category_selector_map
+
+    # Build question selector map
+    for q in questions:
+        preview = f"{q['question_id']}: " + q["turns"][0][:128] + "..."
+        question_selector_map[preview] = q
+        category_selector_map[q["category"]].append(preview)
+
+
 def build_pairwise_browser_tab():
     global question_selector_map, category_selector_map
 
@@ -191,11 +201,6 @@ def build_pairwise_browser_tab():
     num_turns = 2
     side_names = ["A", "B"]
 
-    # Build question selector map
-    for q in questions:
-        preview = f"{q['question_id']}: " + q["turns"][0][:128] + "..."
-        question_selector_map[preview] = q
-        category_selector_map[q["category"]].append(preview)
     question_selector_choices = list(question_selector_map.keys())
     category_selector_choices = list(category_selector_map.keys())
 
@@ -269,11 +274,6 @@ def build_single_answer_browser_tab():
     num_turns = 2
     side_names = ["A"]
 
-    # # Build question selector map
-    # for q in questions:
-    #     preview = f"{q['question_id']}: " + q["turns"][0][:128] + "..."
-    #     question_selector_map[preview] = q
-    #     category_selector_map[q["category"]].append(preview)
     question_selector_choices = list(question_selector_map.keys())
     category_selector_choices = list(category_selector_map.keys())
 
@@ -337,26 +337,6 @@ def build_single_answer_browser_tab():
 
 
 block_css = """
-#notice_markdown {
-    font-size: 104%
-}
-#notice_markdown th {
-    display: none;
-}
-#notice_markdown td {
-    padding-top: 6px;
-    padding-bottom: 6px;
-}
-#leaderboard_markdown {
-    font-size: 104%
-}
-#leaderboard_markdown td {
-    padding-top: 6px;
-    padding-bottom: 6px;
-}
-#leaderboard_dataframe td {
-    line-height: 0.1em;
-}
 #user_question_1 {
     background-color: #DEEBF7;
 }
@@ -389,10 +369,11 @@ def build_demo():
 The code to generate answers and judgments is at [fastchat.llm_judge](https://github.com/lm-sys/FastChat/tree/main/fastchat/llm_judge).
 """
         )
-        with gr.Tab("Pairwise Comparison"):
-            (category_selector,) = build_pairwise_browser_tab()
+        build_question_selector_map()
         with gr.Tab("Single Answer Grading"):
-            (category_selector2,) = build_single_answer_browser_tab()
+            (category_selector,) = build_single_answer_browser_tab()
+        with gr.Tab("Pairwise Comparison"):
+            (category_selector2,) = build_pairwise_browser_tab()
         demo.load(load_demo, [], [category_selector])
         demo.load(load_demo, [], [category_selector2])
 
