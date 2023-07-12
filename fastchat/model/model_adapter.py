f6697ef0ce68f71fc6e6219c7046865c5f460710
CrazyShipOne <>
2023-07-12 15:38:53 +0800

    improve model adapter matching by prioritzing last folder name

--- fastchat/model/model_adapter.py
+++ fastchat/model/model_adapter.py
@@ -97,9 +97,21 @@ def register_model_adapter(cls):
 @cache
 def get_model_adapter(model_path: str) -> BaseModelAdapter:
     """Get a model adapter for a model_path."""
+    model_last_folder_path = os.path.basename(os.path.normpath(model_path))
+
+    adapter_matched_full_path = None
+    adapter_matched_last_path = None
+
     for adapter in model_adapters:
-        if adapter.match(model_path):
-            return adapter
+        if adapter.match(model_last_folder_path) and type(adapter) != BaseModelAdapter :
+            adapter_matched_last_path = adapter
+        elif adapter.match(model_path) and not adapter_matched_full_path:
+            adapter_matched_full_path = adapter
+        if adapter_matched_last_path and adapter_matched_full_path:
+            break
+ 
+    if adapter_matched_full_path or adapter_matched_last_path:
+        return adapter_matched_last_path if adapter_matched_last_path else adapter_matched_full_path
     raise ValueError(f"No valid model adapter for {model_path}")
 
 
