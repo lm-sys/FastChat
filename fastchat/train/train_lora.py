9250ff10f2b097b716e7d1c3ff91e85a87661e2a
airoura163 <airoura@163.com>
2023-07-18 14:06:00 +0800

    avoid to import flash_attn

--- fastchat/train/train_lora.py
+++ fastchat/train/train_lora.py
@@ -35,8 +35,12 @@ from fastchat.train.train import (
     make_supervised_data_module,
 )
 
-from fastchat.train.llama_flash_attn_monkey_patch import (
-    replace_llama_attn_with_flash_attn,
+# from fastchat.train.llama_flash_attn_monkey_patch import (
+#     replace_llama_attn_with_flash_attn,
+# )
+
+from fastchat.train.llama_xformers_attn_monkey_patch import (
+    replace_llama_attn_with_xformers_attn,
 )
 
 
