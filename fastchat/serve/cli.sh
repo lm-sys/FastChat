276de35ce9f205609d7a70bdbeeac13869ab8ee7
minhvn <minhvn@rikkeisoft.com>
2023-07-26 09:18:21 +0000

    update model-path

--- fastchat/serve/cli.sh
+++ fastchat/serve/cli.sh
@@ -1,6 +1,6 @@
-CUDA_VISIBLE_DEVICES=1,2,3,4,5 \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 \
 python3 -m fastchat.serve.cli \
-    --model ~/.cache/huggingface/hub/models--lmsys--vicuna-7b-v1.3/snapshots/RikkeiGPT-vicuna-7b-v1.3 \
+    --model peft_lora_model \
     --style rich \
     --debug \
 
--- fastchat/serve/model_worker.sh
+++ fastchat/serve/model_worker.sh
@@ -1,2 +1,2 @@
 python3 -m fastchat.serve.model_worker \
-         --model-path /media/2T_HDD1/minhvn/.cache/huggingface/hub/models--lmsys--vicuna-7b-v1.3/snapshots/RikkeiGPT-vicuna-7b-v1.3 \
+         --model-path peft_lora_model \
