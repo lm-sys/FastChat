937716d3740ba652df90cb7fb363c7183d51053f
ruanslv <ruanslv@gmail.com>
2023-08-05 02:08:47 -0400

    Remove Llama2 default sys prompt (#2162)

--- fastchat/conversation.py
+++ fastchat/conversation.py
@@ -850,11 +850,6 @@ register_conv_template(
     Conversation(
         name="llama-2",
         system_template="[INST] <<SYS>>\n{system_message}\n<</SYS>>\n\n",
-        system_message="You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. "
-        "Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. "
-        "Please ensure that your responses are socially unbiased and positive in nature.\n\n"
-        "If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. "
-        "If you don't know the answer to a question, please don't share false information.",
         roles=("[INST]", "[/INST]"),
         messages=(),
         offset=0,
