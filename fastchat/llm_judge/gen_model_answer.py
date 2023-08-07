cb7c3c5241888d6a3f4d10287f34a6f313f8f476
Manli Shu <azure.Shu1997@gmail.com>
2023-08-06 18:47:45 -0700

    Fix the slicing issue when no `stop_str` is found.

--- fastchat/llm_judge/gen_model_answer.py
+++ fastchat/llm_judge/gen_model_answer.py
@@ -126,7 +126,8 @@ def get_model_answers(
                         spaces_between_special_tokens=False,
                     )
                     if conv.stop_str:
-                        output = output[: output.find(conv.stop_str)]
+                        if output.find(conv.stop_str) > 0:
+                            output = output[: output.find(conv.stop_str)]
                     for special_token in tokenizer.special_tokens_map.values():
                         if isinstance(special_token, list):
                             for special_tok in special_token:
