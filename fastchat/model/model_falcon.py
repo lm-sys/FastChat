20f1d6dc907887402cf5a889ff673631fecfdfdf
Lianmin Zheng <lianminzheng@gmail.com>
2023-08-08 07:48:25 -0700

    Fix dependencies and styles (#2184)

--- fastchat/model/model_falcon.py
+++ fastchat/model/model_falcon.py
@@ -8,8 +8,6 @@ from transformers import TextIteratorStreamer, GenerationConfig
 
 from fastchat.utils import is_partial_stop
 
-transformers.logging.set_verbosity_error()
-
 
 @torch.inference_mode()
 def generate_stream_falcon(
