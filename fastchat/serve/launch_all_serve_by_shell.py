9d5f1857ec981279dc230e64fd30a748c507f26b
hzg0601 <hzg0601@163.com>
2023-08-08 13:52:16 +0800

    formatted

--- fastchat/serve/launch_all_serve_by_shell.py
+++ fastchat/serve/launch_all_serve_by_shell.py
@@ -9,7 +9,8 @@ The key mechanism behind this scripts is:
 Note that a few of non-critical `fastchat.serve` cmd options are not supported currently.
 """
 import sys
-import os 
+import os
+
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 import subprocess
@@ -19,16 +20,18 @@ import argparse
 LOGDIR = "./logs/"
 
 if not os.path.exists(LOGDIR):
-     os.makedirs(LOGDIR)
+    os.makedirs(LOGDIR)
 
 parser = argparse.ArgumentParser()
-#------multi worker-----------------
-parser.add_argument('--model-path-address',
-                    default="THUDM/chatglm2-6b@localhost@20002",
-                    nargs="+",
-                    type=str,
-                    help="model path, host, and port, formatted as model-path@host@port")
-#---------------controller-------------------------
+# ------multi worker-----------------
+parser.add_argument(
+    "--model-path-address",
+    default="THUDM/chatglm2-6b@localhost@20002",
+    nargs="+",
+    type=str,
+    help="model path, host, and port, formatted as model-path@host@port",
+)
+# ---------------controller-------------------------
 
 parser.add_argument("--controller-host", type=str, default="localhost")
 parser.add_argument("--controller-port", type=int, default=21001)
@@ -38,9 +41,9 @@ parser.add_argument(
     choices=["lottery", "shortest_queue"],
     default="shortest_queue",
 )
-controller_args = ["controller-host","controller-port","dispatch-method"]
+controller_args = ["controller-host", "controller-port", "dispatch-method"]
 
-#----------------------worker------------------------------------------
+# ----------------------worker------------------------------------------
 
 parser.add_argument("--worker-host", type=str, default="localhost")
 parser.add_argument("--worker-port", type=int, default=21002)
@@ -79,9 +82,7 @@ parser.add_argument(
     type=str,
     help="The maximum memory per gpu. Use a string like '13Gib'",
 )
-parser.add_argument(
-    "--load-8bit", action="store_true", help="Use 8-bit quantization"
-)
+parser.add_argument("--load-8bit", action="store_true", help="Use 8-bit quantization")
 parser.add_argument(
     "--cpu-offloading",
     action="store_true",
@@ -126,15 +127,27 @@ parser.add_argument("--stream-interval", type=int, default=2)
 parser.add_argument("--no-register", action="store_true")
 
 worker_args = [
-                "worker-host","worker-port",
-                "model-path","revision","device","gpus","num-gpus",
-               "max-gpu-memory","load-8bit","cpu-offloading",
-               "gptq-ckpt","gptq-wbits","gptq-groupsize",
-               "gptq-act-order","model-names","limit-worker-concurrency",
-               "stream-interval","no-register",
-               "controller-address"
-               ]
-#-----------------openai server---------------------------
+    "worker-host",
+    "worker-port",
+    "model-path",
+    "revision",
+    "device",
+    "gpus",
+    "num-gpus",
+    "max-gpu-memory",
+    "load-8bit",
+    "cpu-offloading",
+    "gptq-ckpt",
+    "gptq-wbits",
+    "gptq-groupsize",
+    "gptq-act-order",
+    "model-names",
+    "limit-worker-concurrency",
+    "stream-interval",
+    "no-register",
+    "controller-address",
+]
+# -----------------openai server---------------------------
 
 parser.add_argument("--server-host", type=str, default="localhost", help="host name")
 parser.add_argument("--server-port", type=int, default=8001, help="port number")
@@ -155,14 +168,20 @@ parser.add_argument(
     type=lambda s: s.split(","),
     help="Optional list of comma separated API keys",
 )
-server_args = ["server-host","server-port","allow-credentials","api-keys",
-               "controller-address"
-               ]
+server_args = [
+    "server-host",
+    "server-port",
+    "allow-credentials",
+    "api-keys",
+    "controller-address",
+]
 
 args = parser.parse_args()
 
-args = argparse.Namespace(**vars(args),**{"controller-address":
-                                          f"http://{args.controller_host}:{args.controller_port}"})
+args = argparse.Namespace(
+    **vars(args),
+    **{"controller-address": f"http://{args.controller_host}:{args.controller_port}"},
+)
 
 if args.gpus:
     if len(args.gpus.split(",")) < args.num_gpus:
@@ -175,7 +194,7 @@ if args.gpus:
 # 1, cmd options
 # 2,LOGDIR
 # 3, log file name
-base_launch_sh = "nohup python3 -m fastchat.serve.{0} {1} >{2}/{3}.log 2>&1 &" 
+base_launch_sh = "nohup python3 -m fastchat.serve.{0} {1} >{2}/{3}.log 2>&1 &"
 
 # 0 LOGDIR
 #! 1 log file name
@@ -187,22 +206,24 @@ base_check_sh = """while [ `grep -c "Uvicorn running on" {0}/{1}.log` -eq '0' ];
                 echo '{2} running' """
 
 
-def string_args(args,args_list):
-
+def string_args(args, args_list):
     args_str = ""
     for key, value in args._get_kwargs():
-
-        key = key.replace("_","-") 
+        key = key.replace("_", "-")
         if key not in args_list:
-             continue
+            continue
 
-        key = key.split("-")[-1] if re.search("port|host",key) else key 
+        key = key.split("-")[-1] if re.search("port|host", key) else key
         if not value:
             pass
         # 1==True ->  True
-        elif isinstance(value,bool) and value == True:
+        elif isinstance(value, bool) and value == True:
             args_str += f" --{key} "
-        elif isinstance(value, list) or isinstance(value, tuple) or isinstance(value, set):
+        elif (
+            isinstance(value, list)
+            or isinstance(value, tuple)
+            or isinstance(value, set)
+        ):
             value = " ".join(value)
             args_str += f" --{key} {value} "
         else:
@@ -210,39 +231,54 @@ def string_args(args,args_list):
 
     return args_str
 
-def launch_worker(item):
-            log_name = item.split("/")[-1].split("\\")[-1].replace("-","_").replace("@","_").replace(".","_")
-            
-            args.model_path,args.worker_host, args.worker_port = item.split("@")
-            print("*"*80)        
-            worker_str_args = string_args(args,worker_args)
-            print(worker_str_args)
-            worker_sh = base_launch_sh.format("model_worker",worker_str_args,LOGDIR,f"worker_{log_name}")
-            worker_check_sh = base_check_sh.format(LOGDIR,f"worker_{log_name}","model_worker")
-            subprocess.run(worker_sh,shell=True,check=True)
-            subprocess.run(worker_check_sh,shell=True,check=True)
 
+def launch_worker(item):
+    log_name = (
+        item.split("/")[-1]
+        .split("\\")[-1]
+        .replace("-", "_")
+        .replace("@", "_")
+        .replace(".", "_")
+    )
+
+    args.model_path, args.worker_host, args.worker_port = item.split("@")
+    print("*" * 80)
+    worker_str_args = string_args(args, worker_args)
+    print(worker_str_args)
+    worker_sh = base_launch_sh.format(
+        "model_worker", worker_str_args, LOGDIR, f"worker_{log_name}"
+    )
+    worker_check_sh = base_check_sh.format(LOGDIR, f"worker_{log_name}", "model_worker")
+    subprocess.run(worker_sh, shell=True, check=True)
+    subprocess.run(worker_check_sh, shell=True, check=True)
 
 
 def launch_all():
-    controller_str_args = string_args(args,controller_args)
-    controller_sh = base_launch_sh.format("controller",controller_str_args,LOGDIR,"controller")
-    controller_check_sh = base_check_sh.format(LOGDIR,"controller","controller")
-    subprocess.run(controller_sh,shell=True,check=True)
-    subprocess.run(controller_check_sh,shell=True,check=True)
+    controller_str_args = string_args(args, controller_args)
+    controller_sh = base_launch_sh.format(
+        "controller", controller_str_args, LOGDIR, "controller"
+    )
+    controller_check_sh = base_check_sh.format(LOGDIR, "controller", "controller")
+    subprocess.run(controller_sh, shell=True, check=True)
+    subprocess.run(controller_check_sh, shell=True, check=True)
 
     if isinstance(args.model_path_address, str):
         launch_worker(args.model_path_address)
     else:
-        for idx,item in enumerate(args.model_path_address):
+        for idx, item in enumerate(args.model_path_address):
             print(f"loading {idx}th model:{item}")
             launch_worker(item)
-    
-    server_str_args = string_args(args,server_args)
-    server_sh = base_launch_sh.format("openai_api_server",server_str_args,LOGDIR,"openai_api_server")
-    server_check_sh = base_check_sh.format(LOGDIR,"openai_api_server","openai_api_server")
-    subprocess.run(server_sh,shell=True,check=True)
-    subprocess.run(server_check_sh,shell=True,check=True)
+
+    server_str_args = string_args(args, server_args)
+    server_sh = base_launch_sh.format(
+        "openai_api_server", server_str_args, LOGDIR, "openai_api_server"
+    )
+    server_check_sh = base_check_sh.format(
+        LOGDIR, "openai_api_server", "openai_api_server"
+    )
+    subprocess.run(server_sh, shell=True, check=True)
+    subprocess.run(server_check_sh, shell=True, check=True)
+
 
 if __name__ == "__main__":
     launch_all()
