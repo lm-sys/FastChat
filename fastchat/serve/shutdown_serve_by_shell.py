07d718cee0512f78eeb6c87d7c497ca959788193
hzg0601 <hzg0601@163.com>
2023-08-10 09:20:09 +0800

    add shutdown_serve_by_shell.py

---
+++ fastchat/serve/shutdown_serve_by_shell.py
@@ -0,0 +1,27 @@
+"""
+Usage：
+python llm_api_shutdown.py --serve all
+options: "all","controller","model_worker","openai_api_server"， `all` means to stop all related serves 
+"""
+import sys
+import os 
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--serve",choices=["all","controller","model_worker","openai_api_server"])
+
+args = parser.parse_args()
+
+base_shell = "ps -eo user,pid,cmd|grep fastchat.serve{}|grep -v grep|awk '{print $2}'|xargs kill -9"
+
+if args.serve == "all":
+    shell_script = base_shell.format("")
+else:
+    serve = f".{args.serve}"
+    shell_script = base_shell.format(serve)
+
+subprocess.run(shell_script,shell=True,check=True)
+print(f"llm api sever --{args.serve} has been shutdown!")
