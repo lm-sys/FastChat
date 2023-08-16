dd78764b63bb48ab6ab8a16f51588cd39969a477
liunux4odoo <liunu@qq.com>
2023-08-16 21:19:55 +0800

    make fastchat api server run in multiprocessing easilly: user can import create_xxx, app from corresponding serve module, and run the app[s] in sub processes using uvicorn.

--- fastchat/serve/model_worker.py
+++ fastchat/serve/model_worker.py
@@ -92,7 +92,7 @@ class BaseModelWorker:
     def init_heart_beat(self):
         self.register_to_controller()
         self.heart_beat_thread = threading.Thread(
-            target=heart_beat_worker, args=(self,)
+            target=heart_beat_worker, args=(self,), daemon=True,
         )
         self.heart_beat_thread.start()
 
@@ -417,7 +417,7 @@ async def api_model_details(request: Request):
     return {"context_length": worker.context_len}
 
 
-if __name__ == "__main__":
+def create_model_worker():
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, default="localhost")
     parser.add_argument("--port", type=int, default=21002)
@@ -482,4 +482,9 @@ if __name__ == "__main__":
         stream_interval=args.stream_interval,
         conv_template=args.conv_template,
     )
+    return args, worker
+
+
+if __name__ == "__main__":
+    args, worker = create_model_worker()
     uvicorn.run(app, host=args.host, port=args.port, log_level="info")
