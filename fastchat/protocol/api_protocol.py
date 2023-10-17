3b9bbbbe3a8cebd5085b13d38a7156f27ef4b933
bodza <bodza@qnovi.de>
2023-10-17 17:57:10 +0200

    Fixed top_k definition

--- fastchat/protocol/api_protocol.py
+++ fastchat/protocol/api_protocol.py
@@ -53,15 +53,15 @@ class APIChatCompletionRequest(BaseModel):
     messages: Union[str, List[Dict[str, str]]]
     temperature: Optional[float] = 0.7
     top_p: Optional[float] = 1.0
-    top_k: Optional[int] = 1.0
+    top_k: Optional[int] = 1
     n: Optional[int] = 1
     max_tokens: Optional[int] = None
     stop: Optional[Union[str, List[str]]] = None
     stream: Optional[bool] = False
     user: Optional[str] = None
     repetition_penalty: Optional[float] = 1.0
-    frequency_penalty: Optional[float] = 1.0
-    presence_penalty: Optional[float] = 1.0
+    frequency_penalty: Optional[float] = 0.0
+    presence_penalty: Optional[float] = 0.0
 
 
 class ChatMessage(BaseModel):
