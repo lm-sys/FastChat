6ef081aa527e06fc82f5fc64c6ce3a047dff8c76
Zhang Wenbin <22811973@qq.com>
2023-11-03 14:01:21 +0800

    Update gradio_web_server.py

--- fastchat/serve/gradio_web_server.py
+++ fastchat/serve/gradio_web_server.py
@@ -520,7 +520,7 @@ def get_model_description_md(models):
         if minfo.simple_name in visited:
             continue
         visited.add(minfo.simple_name)
-        one_model_md = f"[{minfo.simple_name}]({minfo.link}): {minfo.description}"
+        one_model_md = f"[{minfo.simple_name}]({minfo.link})"
 
         if ct % 3 == 0:
             model_description_md += "|"
