40bc583f57fd783a58c8c13d186530c6d063afe3
Tim Li <tianleli@berkeley.edu>
2023-11-07 22:00:50 -0800

    change name

--- fastchat/llm_judge/gen_api_answer.py
+++ fastchat/llm_judge/gen_api_answer.py
@@ -30,7 +30,7 @@ def get_answer(
     if args.force_temperature is not None:
         temperature = args.force_temperature
     elif "required_temp" in question.keys():
-        temperature = question["required_temp"]
+        temperature = question["required_temperature"]
     elif question["category"] in temperature_config:
         temperature = temperature_config[question["category"]]
     else:
