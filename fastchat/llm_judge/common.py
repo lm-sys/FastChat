013bec866a3a053d32ae21d6f906d84aa4c06d57
YuyaYAMAMOTO <dr_jingles@mac.com>
2023-11-19 13:10:36 +0900

    openai.error.APIError -> openai.APIError

--- fastchat/llm_judge/common.py
+++ fastchat/llm_judge/common.py
@@ -417,7 +417,7 @@ def chat_compeletion_openai(model, conv, temperature, max_tokens, api_dict=None)
             )
             output = response["choices"][0]["message"]["content"]
             break
-        except openai.error.OpenAIError as e:
+        except openai.APIError as e:
             print(type(e), e)
             time.sleep(API_RETRY_SLEEP)
 
