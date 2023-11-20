2d4f93cb4a97dce79d9ff8a5de17927ba8055c81
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-11-20 16:51:17 +0800

    add vllm

--- scripts/infer_answer_vllm.sh
+++ scripts/infer_answer_vllm.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+export PYTHONPATH="/root/autodl-tmp/software/FastChat/fastchat/llm_judge:$PYTHONPATH"
+
 python fastchat/llm_judge/gen_model_answer.py --model-path /root/autodl-tmp/model/qwen/Qwen-7B-Chat --model-id qwen-7b-chat --bench-name moral_bench
