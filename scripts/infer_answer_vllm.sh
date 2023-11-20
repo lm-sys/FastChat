8238202703dad4a923741ebb232cc6376bcceb08
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-11-20 17:05:08 +0800

    add vllm

--- scripts/infer_answer_vllm.sh
+++ scripts/infer_answer_vllm.sh
@@ -4,6 +4,8 @@ model_name=$1
 model_id=$2
 data_id=$3
 
+echo $data_id
+
 cd /root/autodl-tmp/software/FastChat/fastchat/llm_judge
 
 python gen_model_answer.py --model-path $model_name --model-id $model_id --bench-name $data_id
