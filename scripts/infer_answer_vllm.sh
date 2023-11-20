b62bf9a4341630a0ea07ed75492970034cf441d7
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-11-20 19:39:49 +0800

    1

--- scripts/infer_answer_vllm.sh
+++ scripts/infer_answer_vllm.sh
@@ -8,6 +8,7 @@ echo $data_id
 
 export PYTHONPATH="/home/workspace/FastChat/fastchat:$PYTHONPATH"
 
+
 cd /home/workspace/FastChat/fastchat/llm_judge
 
 python gen_model_answer.py --model-path $model_name --model-id $model_id --bench-name $data_id
