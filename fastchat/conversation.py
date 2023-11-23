bf4f163d4fa3424385e574f088c5ed13e583bbbd
Wei-Lin Chiang <weichiang@berkeley.edu>
2023-11-23 07:37:00 +0000

    fix

--- fastchat/conversation.py
+++ fastchat/conversation.py
@@ -1028,7 +1028,7 @@ register_conv_template(
         roles=("<|im_start|>user", "<|im_start|>assistant"),
         sep_style=SeparatorStyle.CHATML,
         sep="<|im_end|>",
-        stop_token_ids=[2, 6, 7, 8],  # "<|endoftext|>", "<|im_start|>", "<|im_end|>"
+        stop_token_ids=[2, 6, 7, 8],  # "<|endoftext|>", "<|im_start|>", "<|im_end|>", "<|im_sep|>"
         stop_str="<|endoftext|>",
     )
 )
