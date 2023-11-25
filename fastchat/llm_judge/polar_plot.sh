8b443ad5548dfae12c50666341b5fd7985ba90f3
lionelchg <Cheng.Lionel@bcg.com>
2023-11-25 22:55:06 +0100

    Change location of plot configurations

--- fastchat/llm_judge/polar_plot.sh
+++ fastchat/llm_judge/polar_plot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python polar_plot.py --cfg_fn ./configs/gpt4-judge-all.json
-python polar_plot.py --cfg_fn ./configs/gpt4-judge-gpt.json
-python polar_plot.py --cfg_fn ./configs/gpt4-turbo-judge.json
+python polar_plot.py --cfg_fn ./data/mt_bench/polar_plot_configs/gpt4-judge-all.json
+python polar_plot.py --cfg_fn ./data/mt_bench/polar_plot_configs/gpt4-judge-gpt.json
+python polar_plot.py --cfg_fn ./data/mt_bench/polar_plot_configs/gpt4-turbo-judge.json
