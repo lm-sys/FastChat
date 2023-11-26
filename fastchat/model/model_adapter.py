c51fc1e41631204b7f9b89073ab8df5a2b269552
Lianmin Zheng <lianminzheng@gmail.com>
2023-11-26 00:03:53 -0800

    Update fastchat/model/model_adapter.py

--- fastchat/model/model_adapter.py
+++ fastchat/model/model_adapter.py
@@ -1917,7 +1917,7 @@ class YiAdapter(BaseModelAdapter):
 
     def match(self, model_path: str):
         # use "yi-" instead of "yi" to support Yi Series Models, because "yi" is usually a sub-string of certain words.
-        return "yi-" in model_path.lower()
+        return "yi-" in model_path.lower() and "chat" in model_path.lower()
 
     def get_default_conv_template(self, model_path: str) -> Conversation:
         return get_conv_template("Yi-34b-chat")
