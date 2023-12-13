e8c3a6ef1b243e2a81486712bda6e1f881e3a2e6
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-12-13 16:09:50 +0800

    1

--- fastchat/serve/flask/app_5004.py
+++ fastchat/serve/flask/app_5004.py
@@ -1,3 +1,7 @@
+import json
+import os
+from collections import defaultdict
+
 from flask import Flask, request, jsonify
 import subprocess
 import random
@@ -13,38 +17,75 @@ def generate_random_identifier():
     return ''.join(random.choice(chars) for _ in range(16))
 
 
+def read_jsonl_files(directory):
+    file_dict = {}  # 用于存储文件内容的字典
+    
+    # 检查目录是否存在
+    if not os.path.exists(directory):
+        print(f"目录 '{directory}' 不存在")
+        return file_dict
+    
+    # 获取目录下的所有文件
+    files = os.listdir(directory)
+    
+    # 遍历文件列表
+    for filename in files:
+        if filename.endswith(".jsonl"):  # 确保文件以.jsonl结尾
+            file_path = os.path.join(directory, filename)
+            with open(file_path, "r", encoding="utf-8") as file:
+                content = [json.loads(line) for line in file.readlines()]
+                file_dict[filename] = content
+    
+    return file_dict
+
 app = Flask(__name__)
 
 
-@app.route('/judgement', methods=['POST'])
+@app.route('/judge', methods=['POST'])
 def run_script_judgement():
     data = request.json
     # Validate input data
     if not all(key in data for key in ['judgement_model', 'mode', 'answer_list']):
         return jsonify({"error": "Missing required fields in the request"}), 400
     
-    JUDGE_MODEL = data.get('judgement_model')
-    MODE = data.get('mode')
     DATA_ID = data.get('data_id')
-    ANSWER_LIST = data.get('answer_list')
-    
-    command = f"sh /home/Userlist/madehua/code/fc/fastchat/llm_judge/judgement.sh \"{JUDGE_MODEL}\" \"{MODE}\" \"{DATA_ID}\" \"{ANSWER_LIST}\" "
-    
-    start_time = get_start_time()
-    subprocess.check_call(command, shell=True)
-    end_time = get_end_time()
-    
-    output_name = JUDGE_MODEL
-    ANSWER_LIST = ANSWER_LIST.split()
-    for answer in ANSWER_LIST:
-        output_name += ("_" + answer)
-    output_file = f'/home/Userlist/madehua/code/fc/fastchat/llm_judge/data/single_turn/model_judgment/{output_name}_{MODE}.jsonl'
-    
-    return jsonify({"outputfile": output_file,
-                    "time_start": start_time,
-                    "time_end": end_time}
-                   )
 
+    directory_path = "/home/workspace/FastChat/fastchat/llm_judge/data/" + DATA_ID + "/model_answer"
+    result_dict = read_jsonl_files(directory_path)
+    score_result = {}
+    for model in result_dict:
+        dd0 = defaultdict(list)
+        dd1 = {}
+        model_result = result_dict[model]
+        for answer in model_result:
+            category = answer["category"].split('|||')[0]
+            pred = answer["choices"][0]["turns"][0].split('<|im_end|>')[0]
+            pred_counts = {option: pred.count(option) for option in ['A', 'B', 'C', 'D']}
+            refer_counts = {option: answer["reference_answer"].count(option) for option in ['A', 'B', 'C', 'D']}
+            if all([pred_counts[option] == refer_counts[option] for option in ['A', 'B', 'C', 'D']]):
+                status = True
+            else:
+                status = False
+            dd0[category].append(status)
+        for k, v in dd0.items():
+            dd1[k] = (sum(v) / len(v), sum(v), len(v))
+    
+        print(model, dd1)
+        s0 = sum([v[1] for v in dd1.values()])
+        s1 = sum([v[2] for v in dd1.values()])
+        score_result.update({model: (s0, s1, s0 / s1)})
+
+    try:
+        start_time = get_start_time()
+        end_time = get_end_time()
+        result = {"output": score_result,
+                  "data_id": DATA_ID,
+                  "time_start": start_time,
+                  "time_end": end_time}
+        return jsonify(result)
+    except subprocess.CalledProcessError:
+        return jsonify({"error": "Script execution failed"}), 500
+    
 
 @app.route('/generate', methods=['POST'])
 def run_script_generate():
