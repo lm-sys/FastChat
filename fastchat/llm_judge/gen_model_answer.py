46711eac289e795afc0fcd9d99a29b3a5964c29b
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2023-12-20 16:14:25 +0800

    1

--- fastchat/llm_judge/gen_model_answer.py
+++ fastchat/llm_judge/gen_model_answer.py
@@ -118,7 +118,7 @@ def get_model_answers(
         # print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
 
         os.makedirs(os.path.dirname(answer_file), exist_ok=True)
-        with open(os.path.expanduser(answer_file), "w") as fout:
+        with open(os.path.expanduser(answer_file), "a") as fout:
             ans_json = {
                 "question_id": question["question_id"],
                 "answer_id": shortuuid.uuid(),
