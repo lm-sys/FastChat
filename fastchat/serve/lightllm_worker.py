b701e5acb6993ad6c0f903eed0603bfba021196a
Elsa <zeyugao@outlook.com>
2024-01-06 16:58:19 +0800

    Working lightllm worker

--- fastchat/serve/lightllm_worker.py
+++ fastchat/serve/lightllm_worker.py
@@ -8,11 +8,13 @@ import argparse
 import asyncio
 import json
 import os
+import torch
+import uvicorn
+
 from typing import List
 
 from fastapi import FastAPI, Request, BackgroundTasks
 from fastapi.responses import StreamingResponse, JSONResponse
-import uvicorn
 
 from fastchat.serve.base_model_worker import BaseModelWorker
 from fastchat.serve.model_worker import (
@@ -20,7 +22,6 @@ from fastchat.serve.model_worker import (
     worker_id,
 )
 
-from typing import AsyncGenerator
 
 from lightllm.server.sampling_params import SamplingParams
 from lightllm.server.multimodal_params import MultimodalParams
@@ -39,6 +40,7 @@ from lightllm.utils.start_utils import start_submodule_processes
 from fastchat.utils import get_context_length, is_partial_stop
 
 app = FastAPI()
+g_id_gen = ReqIDGenerator()
 
 
 class LightLLMWorker(BaseModelWorker):
@@ -66,13 +68,12 @@ class LightLLMWorker(BaseModelWorker):
         )
 
         logger.info(
-            f"Loading the model {self.model_names} on worker {worker_id}, worker type: vLLM worker..."
+            f"Loading the model {self.model_names} on worker {worker_id}, worker type: LightLLM worker..."
         )
         self.tokenizer = tokenizer
         self.context_len = context_len
 
         self.is_first = True
-        self.g_id_gen = ReqIDGenerator()
 
         if not no_register:
             self.init_heart_beat()
@@ -87,14 +88,13 @@ class LightLLMWorker(BaseModelWorker):
         top_k = params.get("top_k", -1.0)
         presence_penalty = float(params.get("presence_penalty", 0.0))
         frequency_penalty = float(params.get("frequency_penalty", 0.0))
+        repetition_penalty = float(params.get("repetition_penalty", 1.0))
         max_new_tokens = params.get("max_new_tokens", 256)
+        echo = params.get("echo", True)
         stop_str = params.get("stop", None)
         stop_token_ids = params.get("stop_token_ids", None) or []
         if self.tokenizer.eos_token_id is not None:
             stop_token_ids.append(self.tokenizer.eos_token_id)
-        # echo = params.get("echo", True)
-        # use_beam_search = params.get("use_beam_search", False)
-        # best_of = params.get("best_of", None)
 
         # Handle stop_str
         stop = set()
@@ -126,63 +126,113 @@ class LightLLMWorker(BaseModelWorker):
             top_k=top_k,
             presence_penalty=presence_penalty,
             frequency_penalty=frequency_penalty,
+            repetition_penalty=repetition_penalty,
             max_new_tokens=max_new_tokens,
             stop_sequences=list(stop),
         )
         sampling_params.verify()
 
-        request_id = self.g_id_gen.generate_id()
         results_generator = httpserver_manager.generate(
             prompt, sampling_params, request_id, MultimodalParams())
 
-        async def abort_request() -> None:
-            await httpserver_manager.abort(request_id)
-
-        background_tasks = BackgroundTasks()
-        # Abort the request if the client disconnects.
-        background_tasks.add_task(abort_request)
-
+        completion_tokens = 0
+        text_outputs = ''
         async for request_output, metadata, finished in results_generator:
-            print(metadata.keys())
-            ret = {
-                "text": request_output,
-                "error_code": 0,
-                # "usage": {
-                #     "prompt_tokens": prompt_tokens,
-                #     "completion_tokens": completion_tokens,
-                #     "total_tokens": prompt_tokens + completion_tokens,
-                # },
-                "usage": None,
-                "cumulative_logprob": metadata.get("logprob", None),
-                # "finish_reason": request_output.outputs[0].finish_reason
-                # if len(request_output.outputs) == 1
-                # else [output.finish_reason for output in request_output.outputs],
-                "finish_reason": finished,
-            }
-            # ret = {
-            #     "token": {
-            #         "id": metadata.get("id", None),
-            #         "text": request_output,
-            #         "logprob": metadata.get("logprob", None),
-            #         "special": False
-            #     },
-            #     "generated_text": None,
-            #     "finished": finished,
-            #     "details": None
-            # }
+            text_outputs += request_output
 
-            yield ("data:" + json.dumps(ret, ensure_ascii=False) + "\0").encode(
-                "utf-8"
-            )
+            partial_stop = any(is_partial_stop(text_outputs, i) for i in stop)
+            # prevent yielding partial stop sequence
+            if partial_stop:
+                continue
+
+            prompt_tokens = metadata['prompt_tokens']
+            ret = {
+                "text": prompt + text_outputs if echo else text_outputs,
+                "error_code": 0,
+                "usage": {
+                    "prompt_tokens": prompt_tokens,
+                    "completion_tokens": completion_tokens,
+                    "total_tokens": prompt_tokens + completion_tokens,
+                },
+            }
+            finish_reason = None
+            if finished:
+                yield (json.dumps({**ret, "finish_reason": None}, ensure_ascii=False) + "\0").encode("utf-8")
+                finish_reason = "stop"
+            yield (json.dumps({**ret, "finish_reason": finish_reason}, ensure_ascii=False) + "\0").encode("utf-8")
 
     async def generate(self, params):
-        x = b''
         async for x in self.generate_stream(params):
             pass
         return json.loads(x[:-1].decode())
 
 
+def release_worker_semaphore():
+    worker.semaphore.release()
+
+
+def acquire_worker_semaphore():
+    if worker.semaphore is None:
+        worker.semaphore = asyncio.Semaphore(worker.limit_worker_concurrency)
+    return worker.semaphore.acquire()
+
+
+def create_background_tasks(request_id):
+    async def abort_request() -> None:
+        await httpserver_manager.abort(request_id)
+
+    background_tasks = BackgroundTasks()
+    background_tasks.add_task(release_worker_semaphore)
+    background_tasks.add_task(abort_request)
+    return background_tasks
+
+
+@app.post("/worker_generate_stream")
+async def api_generate_stream(request: Request):
+    params = await request.json()
+    await acquire_worker_semaphore()
+    request_id = g_id_gen.generate_id()
+    params["request_id"] = request_id
+    generator = worker.generate_stream(params)
+    background_tasks = create_background_tasks(request_id)
+    return StreamingResponse(generator, background=background_tasks)
+
+
+@app.post("/worker_generate")
+async def api_generate(request: Request):
+    params = await request.json()
+    await acquire_worker_semaphore()
+    request_id = g_id_gen.generate_id()
+    params["request_id"] = request_id
+    output = await worker.generate(params)
+    release_worker_semaphore()
+    await httpserver_manager.abort(request_id)
+    return JSONResponse(output)
+
+
+@app.post("/worker_get_status")
+async def api_get_status(request: Request):
+    return worker.get_status()
+
+
+@app.post("/count_token")
+async def api_count_token(request: Request):
+    params = await request.json()
+    return worker.count_token(params)
+
+
+@app.post("/worker_get_conv_template")
+async def api_get_conv(request: Request):
+    return worker.get_conv_template()
+
+
+@app.post("/model_details")
+async def api_model_details(request: Request):
+    return {"context_length": worker.context_len}
+
+
 if __name__ == "__main__":
+    torch.multiprocessing.set_start_method('spawn')
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, default="127.0.0.1")
     parser.add_argument("--port", type=int, default=8000)
@@ -197,6 +247,13 @@ if __name__ == "__main__":
     parser.add_argument(
         "--conv-template", type=str, default=None, help="Conversation prompt template."
     )
+    parser.add_argument(
+        "--model-names",
+        type=lambda s: s.split(","),
+        help="Optional display comma separated names",
+    )
+    parser.add_argument("--limit-worker-concurrency", type=int, default=1024)
+    parser.add_argument("--no-register", action="store_true")
 
     parser.add_argument("--tokenizer_mode", type=str, default="slow",
                         help="""tokenizer load mode, can be slow or auto, slow mode load fast but run slow, slow mode is good for debug and test,
@@ -213,12 +270,10 @@ if __name__ == "__main__":
                         help="the max size for forward requests in the same time")
     parser.add_argument("--tp", type=int, default=1,
                         help="model tp parral size, the default is 1")
-    parser.add_argument("--max_req_input_len", type=int, default=2048,
-                        help="the max value for req input tokens num")
-    parser.add_argument("--max_req_total_len", type=int, default=2048 + 1024,
-                        help="the max value for req_input_len + req_output_len")
-    parser.add_argument("--nccl_port", type=int, default=28765,
-                        help="the nccl_port to build a distributed environment for PyTorch")
+    parser.add_argument("--max_req_input_len", type=int, default=None,
+                        help="the max value for req input tokens num. If None, it will be derived from the config.")
+    parser.add_argument("--max_req_total_len", type=int, default=None,
+                        help="the max value for req_input_len + req_output_len. If None, it will be derived from the config.")
     parser.add_argument("--mode", type=str, default=[], nargs='+',
                         help="""Model mode: [triton_int8kv | ppl_int8kv | ppl_fp16 | triton_flashdecoding
                         | triton_gqa_attention | triton_gqa_flashdecoding]
@@ -253,8 +308,6 @@ if __name__ == "__main__":
                         help="splitfuse block size")
     parser.add_argument("--prompt_cache_strs", type=str, default=[], nargs='+',
                         help="""prompt cache strs""")
-    parser.add_argument("--enable_multimodal", action='store_true',
-                        help="Whether or not to allow to load additional multimodal models.")
     parser.add_argument("--cache_capacity", type=int, default=200,
                         help="cache server capacity for multimodal resources")
     parser.add_argument("--cache_reserved_ratio", type=float, default=0.5,
@@ -272,6 +325,15 @@ if __name__ == "__main__":
     # 非splitfuse 模式，不支持 prompt cache 特性
     if not args.splitfuse_mode:
         assert len(args.prompt_cache_strs) == 0
+
+    with open(os.path.join(args.model_dir, "config.json"), "r") as f:
+        model_config = json.load(f)
+    context_length = get_context_length(model_config)
+
+    if args.max_req_input_len is None:
+        args.max_req_input_len = context_length - 1
+    if args.max_req_total_len is None:
+        args.max_req_total_len = context_length
 
     assert args.max_req_input_len < args.max_req_total_len
     assert args.max_req_total_len <= args.max_total_token_num
@@ -294,16 +356,13 @@ if __name__ == "__main__":
             batch_max_tokens = max(batch_max_tokens, args.splitfuse_block_size)
             args.batch_max_tokens = batch_max_tokens
 
-    can_use_ports = alloc_can_use_network_port(
-        num=5 + args.tp, used_nccl_port=args.nccl_port
-    )
-    router_port, detokenization_port, httpserver_port, visual_port, cache_port = can_use_ports[
-        0:5]
-    model_rpc_ports = can_use_ports[5:]
+    can_use_ports = alloc_can_use_network_port(num=6 + args.tp)
 
-    if args.enable_multimodal:
-        start_submodule_processes(start_funcs=[start_cache_manager,],
-                                  start_args=[(cache_port, args.cache_capacity, args.cache_reserved_ratio)])
+    assert can_use_ports is not None, "Can not alloc free enough port"
+    router_port, detokenization_port, httpserver_port, visual_port, cache_port, nccl_port = can_use_ports[
+        0:6]
+    args.nccl_port = nccl_port
+    model_rpc_ports = can_use_ports[6:]
 
     global httpserver_manager
     httpserver_manager = HttpServerManager(
@@ -312,30 +371,23 @@ if __name__ == "__main__":
         cache_port=cache_port,
         visual_port=visual_port,
         httpserver_port=httpserver_port,
-        enable_multimodal=args.enable_multimodal,
+        enable_multimodal=False,
     )
 
     start_submodule_processes(start_funcs=[start_router_process, start_detokenization_process],
                               start_args=[(args, router_port, detokenization_port, model_rpc_ports),
                                           (args, detokenization_port, httpserver_port)])
-    if args.enable_multimodal:
-        start_submodule_processes(start_funcs=[start_visual_process,],
-                                  start_args=[(args, router_port, visual_port, cache_port),])
-
-    with open(os.path.join(args.model_dir, "config.json"), "r") as f:
-        model_config = json.load(f)
-
     worker = LightLLMWorker(
         args.controller_address,
         args.worker_address,
         worker_id,
-        args.model_path,
+        args.model_dir,
         args.model_names,
         args.limit_worker_concurrency,
         args.no_register,
         args.conv_template,
         httpserver_manager.tokenizer,
-        get_context_length(model_config),
+        context_length,
     )
 
     uvicorn.run(app, host=args.host, port=args.port, log_level="info")
