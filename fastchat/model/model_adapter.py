78bfebcd30c230e88c3e9c5a6ff61067c6aaf2e4
mulin.lyh <mulin.lyh@taobao.com>
2024-01-18 21:44:38 +0800

    fix format issue

--- fastchat/model/model_adapter.py
+++ fastchat/model/model_adapter.py
@@ -336,7 +336,8 @@ def load_model(
         # lazy import so that modelscope is not required for normal use.
         try:
             from modelscope.hub.snapshot_download import snapshot_download
-            if not os.path.exists(model_path):                
+
+            if not os.path.exists(model_path):
                 model_path = snapshot_download(model_id=model_path, revision=revision)
         except ImportError as e:
             warnings.warn(
