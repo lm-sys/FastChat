089dbb170a1239b8174fc3ada61c25a55d502b25
BabyChouSr <christopherchou@berkeley.edu>
2024-01-24 02:41:58 +0000

    Add back fetch_remote to openai_api_server

--- fastchat/serve/openai_api_server.py
+++ fastchat/serve/openai_api_server.py
@@ -60,13 +60,37 @@ from fastchat.protocol.api_protocol import (
     APITokenCheckResponse,
     APITokenCheckResponseItem,
 )
-from fastchat.utils import build_logger, fetch_remote
+from fastchat.utils import build_logger
 
 logger = build_logger("openai_api_server", "openai_api_server.log")
 
 conv_template_map = {}
 
 fetch_timeout = aiohttp.ClientTimeout(total=3 * 3600)
+
+
+async def fetch_remote(url, pload=None, name=None):
+    async with aiohttp.ClientSession(timeout=fetch_timeout) as session:
+        async with session.post(url, json=pload) as response:
+            chunks = []
+            if response.status != 200:
+                ret = {
+                    "text": f"{response.reason}",
+                    "error_code": ErrorCode.INTERNAL_ERROR,
+                }
+                return json.dumps(ret)
+
+            async for chunk, _ in response.content.iter_chunks():
+                chunks.append(chunk)
+        output = b"".join(chunks)
+
+    if name is not None:
+        res = json.loads(output)
+        if name != "":
+            res = res[name]
+        return res
+
+    return output
 
 
 class AppSettings(BaseSettings):
