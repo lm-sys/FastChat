ef00083bf8191c7d0af18e4965905e92f35aaaf4
Lisa Dunlap <lisabdunlap@gmail.com>
2024-02-13 06:24:42 +0000

    remove filepath dependencies

--- fastchat/serve/gradio_block_arena_vision.py
+++ fastchat/serve/gradio_block_arena_vision.py
@@ -159,14 +159,9 @@ Note: You can only chat with **one image per conversation**. You can upload imag
                 flag_btn = gr.Button(value="⚠️  Flag", interactive=False)
                 regenerate_btn = gr.Button(value="🔄  Regenerate", interactive=False)
                 clear_btn = gr.Button(value="🗑️  Clear", interactive=False)
-<<<<<<< HEAD
 
     if add_promotion_links:
         gr.Markdown(acknowledgment_md, elem_id="ack_markdown")
-=======
-                if random_questions:
-                    random_btn = gr.Button(value="🎲  Random", interactive=True)
->>>>>>> d2b89fb (formatting)
 
     # Register listeners
     btn_list = [upvote_btn, downvote_btn, flag_btn, regenerate_btn, clear_btn]
@@ -222,7 +217,7 @@ Note: You can only chat with **one image per conversation**. You can upload imag
     if random_questions:
         random_btn.click(
             get_vqa_sample,  # First, get the VQA sample
-            [],  # No inputs for get_vqa_sample
+            [],  # Pass the path to the VQA samples
             [textbox, imagebox],  # Outputs are textbox and imagebox
         ).then(clear_history_example, None, [state, chatbot] + btn_list)
 
