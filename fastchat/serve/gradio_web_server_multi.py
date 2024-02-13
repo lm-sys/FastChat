203d34004749860302ad0151f03c8c369b277412
Lianmin Zheng <lianminzheng@gmail.com>
2024-02-13 01:21:43 +0000

    update

--- fastchat/serve/gradio_web_server_multi.py
+++ fastchat/serve/gradio_web_server_multi.py
@@ -128,9 +128,7 @@ window.__gradio_mode__ = "app";
                     models, add_promotion_links=True
                 )
 
-            with gr.Tab(
-                "Vision Direct Chat", id=3, visible=args.multimodal
-            ):
+            with gr.Tab("Vision Direct Chat", id=3, visible=args.multimodal):
                 single_vision_language_model_list = (
                     build_single_vision_language_model_ui(
                         vl_models, add_promotion_links=True
