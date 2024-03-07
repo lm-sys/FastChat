29f3370ce010ae7c46e1cf27cb47614525f13258
Kai Xu <xuk@ibm.com>
2024-03-07 15:23:06 -0500

    feat: support labradorite and merlinite

--- fastchat/model/model_adapter.py
+++ fastchat/model/model_adapter.py
@@ -2267,6 +2267,23 @@ class GemmaAdapter(BaseModelAdapter):
     def get_default_conv_template(self, model_path: str) -> Conversation:
         return get_conv_template("gemma")
 
+class LabradoriteAdapter(BaseModelAdapter):
+    """The model adapter for ibm/labradorite-13b"""
+
+    def match(self, model_path: str):
+        return "labradorite" in model_path.lower()
+    
+    def get_default_conv_template(self, model_path: str) -> Conversation:
+        return get_conv_template("labradorite")
+
+class IBMGenericAdapter(BaseModelAdapter):
+    """The model adapter for ibm/merlinite-7b"""
+
+    def match(self, model_path: str):
+        return 'merlinite' in model_path.lower()
+    
+    def get_default_conv_template(self, model_path: str) -> Conversation:
+        return get_conv_template("ibm-generic")
 
 # Note: the registration order matters.
 # The one registered earlier has a higher matching priority.
@@ -2358,6 +2375,8 @@ register_model_adapter(SteerLMAdapter)
 register_model_adapter(LlavaAdapter)
 register_model_adapter(YuanAdapter)
 register_model_adapter(GemmaAdapter)
+register_model_adapter(LabradoriteAdapter)
+register_model_adapter(IBMGenericAdapter)
 
 # After all adapters, try the default base adapter.
 register_model_adapter(BaseModelAdapter)
