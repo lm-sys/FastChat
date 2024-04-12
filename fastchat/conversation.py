6a98121b87abdc3147f5b9a358e812d6b3bf60cf
Che Zheng <xvblue@gmail.com>
2024-04-12 16:26:49 +0800

    Reka AI model integration (#3235)
    Co-authored-by: Che Zheng <chezheng@reka.ai>

--- fastchat/conversation.py
+++ fastchat/conversation.py
@@ -1630,6 +1630,16 @@ register_conv_template(
     )
 )
 
+register_conv_template(
+    Conversation(
+        name="reka",
+        system_message="",
+        roles=("user", "assistant"),
+        sep_style=None,
+        sep=None,
+    )
+)
+
 
 if __name__ == "__main__":
     from fastchat.conversation import get_conv_template
