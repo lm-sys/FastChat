lm-sys/FastChat

commit 2d9e2a6111ebe2f9ecc334af3fb5356cc6ee41cc

Store Images Remotely on GCS (#3172)

fastchat/serve/gradio_web_server.py
@@ -53,6 +53,7 @@ invisible_btn = gr.Button(interactive=False, visible=False)
 
 controller_url = None
 enable_moderation = False
+use_remote_storage = False
 
 acknowledgment_md = """
 ### Terms of Service
@@ -115,10 +116,11 @@ class State:
         return base
 
 
-def set_global_vars(controller_url_, enable_moderation_):
-    global controller_url, enable_moderation
+def set_global_vars(controller_url_, enable_moderation_, use_remote_storage_):
+    global controller_url, enable_moderation, use_remote_storage
     controller_url = controller_url_
     enable_moderation = enable_moderation_
+    use_remote_storage = use_remote_storage_
 
 
 def get_conv_log_filename():
@@ -356,6 +358,25 @@ def is_limit_reached(model_name, ip):
         return None
 
 
+def upload_image_file_to_gcs(image, filename):
+    from google.cloud import storage
+    import io
+
+    storage_client = storage.Client()
+    # upload file to GCS
+    bucket = storage_client.get_bucket("arena_user_content")
+
+    blob = bucket.blob(f"{filename}")
+    if not blob.exists():
+        buffer = io.BytesIO()
+        image.save(buffer, format="PNG")
+        buffer.seek(0)
+        blob.upload_from_file(buffer, content_type="image/png")
+
+    blob.make_public()
+    return blob.public_url
+
+
 def bot_response(
     state,
     temperature,
@@ -508,16 +529,24 @@ def bot_response(
     # We load the image because gradio accepts base64 but that increases file size by ~1.33x
     loaded_images = [load_image(image) for image in images]
     images_hash = [hashlib.md5(image.tobytes()).hexdigest() for image in loaded_images]
+    image_filenames = []
     for image, hash_str in zip(loaded_images, images_hash):
         t = datetime.datetime.now()
         filename = os.path.join(
-            LOGDIR,
             "serve_images",
             f"{hash_str}.jpg",
         )
-        if not os.path.isfile(filename):
-            os.makedirs(os.path.dirname(filename), exist_ok=True)
-            image.save(filename)
+
+        if use_remote_storage:
+            image_url = upload_image_file_to_gcs(image, filename)
+            image_filenames.append(image_url)
+        else:
+            filename = os.path.join(LOGDIR, filename)
+            if not os.path.isfile(filename):
+                os.makedirs(os.path.dirname(filename), exist_ok=True)
+                image.save(filename)
+
+            image_filenames.append(hash_str)
 
     with open(get_conv_log_filename(), "a") as fout:
         data = {
@@ -533,7 +562,7 @@ def bot_response(
             "finish": round(finish_tstamp, 4),
             "state": state.dict(),
             "ip": get_ip(request),
-            "images": images_hash,
+            "images": image_filenames,
         }
         fout.write(json.dumps(data) + "\n")
 

fastchat/serve/gradio_web_server_multi.py
@@ -235,11 +235,17 @@ if __name__ == "__main__":
         help="the Google Analytics ID",
         default=None,
     )
+    parser.add_argument(
+        "--use-remote-storage",
+        action="store_true",
+        default=False,
+        help="Uploads image files to google cloud storage if set to true",
+    )
     args = parser.parse_args()
     logger.info(f"args: {args}")
 
     # Set global variables
-    set_global_vars(args.controller_url, args.moderate)
+    set_global_vars(args.controller_url, args.moderate, args.use_remote_storage)
     set_global_vars_named(args.moderate)
     set_global_vars_anony(args.moderate)
     models, all_models = get_model_list(

commit 28d427601a2b03a75cf6862b9e84d383aa7a0fbc

Fix bug in gradio_web_server.py
Fix missing flag introduced in https://github.com/lm-sys/FastChat/pull/3172

fastchat/serve/gradio_web_server.py
@@ -989,11 +989,17 @@ if __name__ == "__main__":
         type=str,
         help="Sets the gradio root path, eg /abc/def. Useful when running behind a reverse-proxy or at a custom URL path prefix",
     )
+    parser.add_argument(
+        "--use-remote-storage",
+        action="store_true",
+        default=False,
+        help="Uploads image files to google cloud storage if set to true",
+    )
     args = parser.parse_args()
     logger.info(f"args: {args}")
 
     # Set global variables
-    set_global_vars(args.controller_url, args.moderate)
+    set_global_vars(args.controller_url, args.moderate, args.use_remote_storage)
     models, all_models = get_model_list(
         args.controller_url, args.register_api_endpoint_file, False
     )