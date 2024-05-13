eb17d635d49600fed014b22e7f4dde57a1965823
Teknium <127238744+teknium1@users.noreply.github.com>
2024-05-12 21:53:44 -0700

    Update model_adapter.py

--- fastchat/model/model_adapter.py
+++ fastchat/model/model_adapter.py
@@ -1564,7 +1564,7 @@ class Llama3Adapter(BaseModelAdapter):
     """The model adapter for Llama-3 (e.g., meta-llama/Meta-Llama-3-8B-Instruct)"""
 
     def match(self, model_path: str):
-        return "llama-3" in model_path.lower()
+        return "meta-llama-3" in model_path.lower()
 
     def load_model(self, model_path: str, from_pretrained_kwargs: dict):
         model, tokenizer = super().load_model(model_path, from_pretrained_kwargs)
