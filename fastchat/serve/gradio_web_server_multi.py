65c60950fa2f05b569594002263699ff27cbfa52
Christopher Chou <emu4yolife@gmail.com>
2024-05-16 19:50:48 +0000

    new name

--- fastchat/serve/gradio_web_server_multi.py
+++ fastchat/serve/gradio_web_server_multi.py
@@ -156,7 +156,7 @@ window.__gradio_mode__ = "app";
             )
 
             if args.vision_arena:
-                with gr.Tab("⭐⭐ Vision Arena ⭐⭐", id=3):
+                with gr.Tab("Vision Arena (📣 NEW!)", id=3):
                     with gr.Tab("⚔️  Vision Arena (battle)", id=3):
                         side_by_side_vision_anony_list = (
                             build_side_by_side_vision_ui_anony(
