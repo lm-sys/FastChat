ee47195ef6e2a23e2c06dfd29cedbd7d1917ce8c
lisadunlap <lisabdunlap@gmail.com>
2024-08-05 04:16:30 +0000

    formatting

--- fastchat/serve/gradio_block_arena_vision_anony.py
+++ fastchat/serve/gradio_block_arena_vision_anony.py
@@ -374,7 +374,7 @@ def add_text(
 
 
 def build_side_by_side_vision_ui_anony(text_models, vl_models, random_questions=None):
-    notice_markdown = """
+    notice_markdown = f"""
 # ⚔️  LMSYS Chatbot Arena (Multimodal): Benchmarking LLMs and VLMs in the Wild
 [Blog](https://lmsys.org/blog/2023-05-03-arena/) | [GitHub](https://github.com/lm-sys/FastChat) | [Paper](https://arxiv.org/abs/2403.04132) | [Dataset](https://github.com/lm-sys/FastChat/blob/main/docs/dataset_release.md) | [Twitter](https://twitter.com/lmsysorg) | [Discord](https://discord.gg/HSWAKCrnFx) | [Kaggle Competition](https://www.kaggle.com/competitions/lmsys-chatbot-arena)
 
