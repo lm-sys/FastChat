b9bf4f220cfa706c7af49f981524b0b02f6c7b42
SHIV-anna <shivshankar07123@gmail.com>
2024-08-13 22:46:26 +0530

    added support for 4bit quantization

--- fastchat/model/model_adapter.py
+++ fastchat/model/model_adapter.py
@@ -377,15 +377,16 @@ def load_model(
     ):
         model = ipex.optimize(model, dtype=kwargs["torch_dtype"])
 
-    if (device == "cuda" and num_gpus == 1 and not cpu_offloading) or device in (
-        "mps",
-        "xpu",
-        "npu",
-    ):
-        model.to(device)
+    if not load_4bit:
+        if (device == "cuda" and num_gpus == 1 and not cpu_offloading) or device in (
+            "mps",
+            "xpu",
+            "npu",
+        ):
+            model.to(device)
 
-    if device == "xpu":
-        model = torch.xpu.optimize(model, dtype=kwargs["torch_dtype"], inplace=True)
+        if device == "xpu":
+            model = torch.xpu.optimize(model, dtype=kwargs["torch_dtype"], inplace=True)
 
     if debug:
         print(model)
