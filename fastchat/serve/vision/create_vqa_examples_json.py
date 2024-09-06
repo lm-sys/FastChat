853168f14472dd176cc93776a27713841835ebcd
Lisa Dunlap <lisabdunlap@gmail.com>
2024-09-05 20:20:52 -0700

    Update preset (#3516)

--- fastchat/serve/vision/create_vqa_examples_json.py
+++ fastchat/serve/vision/create_vqa_examples_json.py
@@ -28,7 +28,11 @@ if __name__ == "__main__":
     for dataset_name in dataset_prop.keys():
         with open(f"{args.output_dir}/{dataset_name}/data.json") as f:
             data = json.load(f)
-            dataset_json.extend(np.random.choice(data, dataset_prop[dataset_name]))
+            dataset_json.extend(
+                np.random.choice(
+                    data, min(dataset_prop[dataset_name], len(data)), replace=False
+                )
+            )
 
     with open(f"{args.output_dir}/metadata_sampled.json", "w") as f:
         json.dump(dataset_json, f, indent=4)
