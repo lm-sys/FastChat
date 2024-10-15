7f52ce32146e242d24daf0a56474a322c9183443
Wei-Lin Chiang <weichiang@berkeley.edu>
2024-10-15 17:15:22 +0000

    fix

--- fastchat/serve/monitor/monitor.py
+++ fastchat/serve/monitor/monitor.py
@@ -896,7 +896,7 @@ def get_combined_table(elo_results, model_table_df):
     combined_table = []
     for category in elo_results.keys():
         df = elo_results[category]["leaderboard_table_df"].copy()
-        # # remove deprecated models
+        # remove deprecated models
         df = df.loc[~df.index.isin(deprecated_model_name)]
 
         ranking = recompute_final_ranking(df)
