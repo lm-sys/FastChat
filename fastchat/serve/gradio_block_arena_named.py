a5fda633000ba7fa420fdef1d39685ce7b7ecc47
Tianle (Tim) Li <67527391+CodingWithTim@users.noreply.github.com>
2024-11-14 15:17:18 -0800

    Add Chatbot Arena Web Server Documentation and Fix Latex Display (#3615)
    Co-authored-by: Wei-Lin Chiang <weichiang@berkeley.edu>

--- fastchat/serve/gradio_block_arena_named.py
+++ fastchat/serve/gradio_block_arena_named.py
@@ -358,6 +358,12 @@ def build_side_by_side_ui_named(models):
                         elem_id=f"chatbot",
                         height=650,
                         show_copy_button=True,
+                        latex_delimiters=[
+                            {"left": "$", "right": "$", "display": False},
+                            {"left": "$$", "right": "$$", "display": True},
+                            {"left": r"\(", "right": r"\)", "display": False},
+                            {"left": r"\[", "right": r"\]", "display": True},
+                        ],
                     )
 
     with gr.Row():
