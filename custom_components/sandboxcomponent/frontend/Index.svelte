16c49109d742bf70b658d68523ad2fbde9e7ce4c
digitsisyph <ksqod_code@pm.me>
2024-12-11 22:45:08 +0800

    Add Dark Mode and Size Limit

--- custom_components/sandboxcomponent/frontend/Index.svelte
+++ custom_components/sandboxcomponent/frontend/Index.svelte
@@ -37,20 +37,23 @@
     iframeKey += 1;
   }
 
-  export let width: number = Math.max(min_width ?? 0, 800); // Initialize width to full window width
-  export let height: number = 600; // Initial height in pixels
+  export let width: number | null = null; // Initialize width to full window width
+  export let height: number | null = 400; // Initial height in pixels
   let isResizing = false;
   let startY: number;
   let startX: number;
   let startHeight: number;
   let startWidth: number;
+  let divElement: HTMLElement | null;
 
   function onClickResize(event: MouseEvent) {
     isResizing = true;
     startY = event.clientY;
     startX = event.clientX;
-    startHeight = height;
-    startWidth = width;
+    startHeight = divElement?.clientHeight;
+    startWidth = divElement?.clientWidth;
+    console.log("onClickResize", "startHeight", startHeight);
+    console.log("onClickResize", "startWidth", startWidth);
     window.addEventListener("mousemove", onResizeMouseMove);
   }
 
@@ -65,7 +68,10 @@
         stopResizing();
       } else {
         height = Math.max(100, startHeight + (event.clientY - startY));
-        width = Math.max(min_width ?? 100, startWidth + (event.clientX - startX));
+        width = Math.max(
+          min_width ?? 100,
+          startWidth + (event.clientX - startX)
+        );
       }
     }
   }
@@ -78,6 +84,11 @@
     } else {
       document.exitFullscreen();
     }
+  }
+
+  let isDarkMode = false;
+  function toggleDarkMode() {
+    isDarkMode = !isDarkMode;
   }
 
   onMount(() => {
@@ -101,12 +112,16 @@
 
   <div class:full-screen={isFullScreen}>
     <div
-      class="flex items-center justify-between bg-black border border-gray-300 rounded px-2 py-1 font-mono w-full"
+      class={`flex items-center justify-between border border-gray-300 rounded px-2 py-1 font-mono w-full ${isDarkMode ? "bg-black text-white" : "bg-white text-black"}`}
     >
-      <div class="bg-gray-800 p-2 rounded-t-md flex items-center mr-10">
-        <span class="text-white bg-gray-700 px-3 py-1 rounded-r-md flex-1"
-          >{sandboxUrl}</span
+      <div
+        class={`p-2 rounded-t-md flex items-center mr-10 ${isDarkMode ? "bg-gray-900" : "bg-gray-100"}`}
+      >
+        <span
+          class={`px-3 py-1 rounded-r-md flex-1 ${isDarkMode ? "text-white bg-gray-800" : "text-black bg-gray-200"}`}
         >
+          {sandboxUrl}
+        </span>
       </div>
       <div>
         <!-- Copy URL Button -->
@@ -128,6 +143,43 @@
               d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
             />
           </svg>
+        </button>
+        <!-- Dark Mode Button -->
+        <button
+          on:click={toggleDarkMode}
+          class="px-2 py-1 bg-blue-500 text-white rounded cursor-pointer ml-2"
+        >
+          {#if isDarkMode}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+              />
+            </svg>
+          {:else}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 3v1.5M12 19.5V21M4.22 4.22l1.06 1.06M17.72 17.72l1.06 1.06M3 12h1.5M19.5 12H21M4.22 19.78l1.06-1.06M17.72 6.28l1.06-1.06M12 6.75A5.25 5.25 0 1 0 17.25 12 5.25 5.25 0 0 0 12 6.75z"
+              />
+            </svg>
+          {/if}
         </button>
         <!-- Full-Screen Button -->
         <button
@@ -191,14 +243,20 @@
 
     <div
       class:pending={loading_status?.status === "pending"}
-      class="relative w-full"
-      style={isFullScreen ? `height: 100%; width: 100%` : `height: ${height}px; width: ${width}px;`}
+      class="relative w-full flex flex-col"
+      style={isFullScreen || !width || !height
+        ? `height: 100%; width: 100%; max-width: 100%; max-height: 100%; min-height: 400px;`
+        : `height: ${height}px; width: ${width}px; max-width: 100%; max-height: 100%; min-height: 400px;`}
+      bind:this={divElement}
     >
       {#key iframeKey}
-        <div class="h-full p-10 bg-slate-600 backdrop-blur">
+        <div
+          class={`flex flex-col flex-1 p-10 ${isDarkMode ? "bg-slate-800" : "bg-slate-100"} backdrop-blur`}
+        >
           <IFrameContainer
             min_height={loading_status && loading_status?.status !== "complete"}
             value={sandboxUrl}
+            {isDarkMode}
             {elem_classes}
             {visible}
             on:change={() => gradio.dispatch("change")}
