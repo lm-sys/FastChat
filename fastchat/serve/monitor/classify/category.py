c95bcbec468dc3bcb5a7c34969c05b265309e2fe
Derry Xu <derryxu@berkeley.edu>
2024-12-26 08:50:16 +0000

    remove redundant attributes

--- fastchat/serve/monitor/classify/category.py
+++ fastchat/serve/monitor/classify/category.py
@@ -188,8 +188,6 @@ class CategoryIF(CategoryAPI):
         self.pattern = re.compile(r"<score>([012345])<\/score>")
         self.system_prompt = "You are an AI assistant tasked with determining whether a given user prompt can effectively assess another AI's ability to follow instructions. Your goal is to analyze the prompt and decide if it contains specific, clear instructions that would test an AI's capability to understand and execute directions accurately. Carefully examine the user prompt and consider the following aspects:\n1. Does it contain specific instructions or requirements?\n2. Are there multiple steps or elements the AI needs to address?\n3. Does it ask for a particular format or structure in the response?\n4. Is there a unique or challenging aspect that would test the AI's ability to follow directions precisely?\n\nConsider both the content and the structure of the instructions. A good prompt for assessing instruction-following capabilities should have clear, specific directions that can be objectively evaluated. Think about why this prompt does or does not effectively assess an AI's ability to follow instructions. Consider both the strengths and weaknesses of the prompt in this regard. Output your verdict as a score from 0 to 5:\n0 = Does not evaluate instruction-following ability.\n1 = Ineffective at evaluating instruction-following ability.\n2 = Somewhat effective at evaluating instruction-following ability.\n3 = Effective at evaluating simple instruction-following ability.\n4 = Effective at evaluating more complex instruction-following ability.\n5 = Effective at evaluating advanced instruction-following ability.\n\nPresent your score in the following format:\n<score>[Your score from 0 to 5]</score>.\nDo NOT explain."
         self.prompt_template = "<user_prompt>{PROMPT}</user_prompt>"
-        self.batch_size = 1
-        self.is_parallel = True
 
     def get_score(self, judgment):
         matches = self.pattern.findall(judgment)
@@ -230,8 +228,6 @@ class CategoryMath(CategoryAPI):
         self.pattern = re.compile(r"<decision>(\w+)<\/decision>")
         self.system_prompt = 'You are tasked with determining whether a given user prompt requires an AI assistant to solve a math problem and apply mathematical logic and reasoning.\n\nCarefully analyze the user prompt and consider whether it requires mathematical problem-solving skills to answer correctly. Think about the following aspects:\n\n1. Does it require the application of a specific mathematical concept or formula?\n2. Does the prompt involve numerical calculations or algebraic manipulation or logical reasoning?\n3. Is there a clear mathematical problem to be solved?\n4. Would answering this prompt demonstrate proficiency in a specific area in mathematics?\n\nOutput your verdict in the following format:"<decision>\n[yes/no]\n</decision>". Do NOT explain.'
         self.prompt_template = "<user_prompt>\n{PROMPT}\n</user_prompt>"
-        self.batch_size = 1
-        self.is_parallel = True
 
     def get_score(self, judgment):
         matches = self.pattern.findall(judgment.replace("\n", "").lower())
@@ -268,8 +264,6 @@ class CategoryCreativeWriting(CategoryAPI):
         self.pattern = re.compile(r"<decision>(\w+)<\/decision>")
         self.system_prompt = 'You are tasked with determining whether a given user prompt is asking for creative writing. Creative writing is defined as any form of writing that goes beyond standard professional, journalistic, academic, or technical literature. It typically involves imagination, originality, and expression of thoughts and emotions. Creative writing can include, but is not limited to, the following formats:\n- Fiction (e.g., short stories, novels)\n- Poetry (e.g., sonnets, free verse)\n- Dramatic writing (e.g., screenplays, monologues, scripts)\n- Personal essays (focusing on subjective experiences or narrative storytelling)\n- Songs and lyrics\n\nCarefully analyze the user prompt and consider whether it primarily requires creative writing. Think about the following aspects:\n1. Does the prompt ask for fictional content, speculative scenarios, or the use of imagination to construct narratives?\n2. Does it encourage the expression of thoughts, emotions, or personal experiences beyond mere factual reporting or analysis?\n3. Is it asking for writing in a specific creative format (e.g., story, poem, script, etc)?\n4. Is the primary purpose of the prompt to foster creative expression or originality rather than information delivery, technical documentation, or analytical reasoning?\n5. Does the prompt request stylistic or rhetorical elements often associated with creative writing, such as metaphor, imagery, dialogue, etc?\n6. Does the prompt expect a response in natural language (e.g., sentences, paragraphs) rather than visual, mathematical, or non-linguistic output?\n\nOutput your verdict as either "yes" or "no"in the following format:\n<decision>\n[yes/no]\n</decision>. Do NOT explain.'
         self.prompt_template = "<user_prompt>\n{PROMPT}\n</user_prompt>"
-        self.batch_size = 1
-        self.is_parallel = True
 
     def get_score(self, judgment):
         matches = self.pattern.findall(
@@ -314,8 +308,6 @@ class CategoryRefusalHF(CategoryHF):
         self.classifier = HuggingFaceClassifier(
             model_path="lmarena-ai/RefusalClassifier"
         )
-        self.batch_size = 1
-        self.is_parallel = False
 
     def conv_pre_process_helper(self, conversation):
         conv = []
