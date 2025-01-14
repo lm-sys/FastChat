bd0979aba004ead9e835eb3c44727a56a62d4697
likaixin <likaixin@u.nus.edu>
2025-01-14 18:18:10 +0800

    feat: open sandbox in new page

--- custom_components/sandboxcomponent/frontend/Index.svelte
+++ custom_components/sandboxcomponent/frontend/Index.svelte
@@ -218,6 +218,17 @@
             </svg>
           {/if}
         </button>
+        <!-- Open in new page Button-->
+         <button on:click={() => window.open(sandboxUrl)} class="px-2 py-1 bg-blue-500 text-white rounded cursor-pointer ml-2">
+          <svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 2" viewBox="0 0 24 30" x="0px" y="0px">
+            <defs><style>
+            .cls-1 {
+              stroke-width: 0px;
+            }
+            </style></defs>
+            <path class="cls-1" d="m21,12v6c0,1.6543-1.3457,3-3,3H6c-1.6543,0-3-1.3457-3-3V6c0-1.6543,1.3457-3,3-3h6c.55273,0,1,.44775,1,1s-.44727,1-1,1h-6c-.55176,0-1,.44873-1,1v12c0,.55127.44824,1,1,1h12c.55176,0,1-.44873,1-1v-6c0-.55225.44727-1,1-1s1,.44775,1,1Zm-1-9h-4c-.55273,0-1,.44775-1,1s.44727,1,1,1h1.58594l-9.29297,9.29297c-.39062.39062-.39062,1.02344,0,1.41406.19531.19531.45117.29297.70703.29297s.51172-.09766.70703-.29297l9.29297-9.29297v1.58594c0,.55225.44727,1,1,1s1-.44775,1-1v-4c0-.55225-.44727-1-1-1Z"/>
+          </svg>
+        </button>
         <!-- Refresh Button -->
         <button
           on:click={refreshHTML}
