3046ae758fa0fd55b13b599b4c12d9e21201fbca
Taksh <takshkothari09@gmail.com>
2026-07-16 15:52:09 +0300

    style: black-format test_stream_logger for CI

--- tests/test_stream_logger.py
+++ tests/test_stream_logger.py
@@ -4,7 +4,12 @@ from fastchat.utils import infer_stream_log_level
 
 
 def test_infer_stream_log_level_maps_uvicorn_prefixes():
-    assert infer_stream_log_level("INFO:     Started server", logging.ERROR) == logging.INFO
-    assert infer_stream_log_level("WARNING: deprecated", logging.ERROR) == logging.WARNING
+    assert (
+        infer_stream_log_level("INFO:     Started server", logging.ERROR)
+        == logging.INFO
+    )
+    assert (
+        infer_stream_log_level("WARNING: deprecated", logging.ERROR) == logging.WARNING
+    )
     assert infer_stream_log_level("ERROR: boom", logging.ERROR) == logging.ERROR
     assert infer_stream_log_level("plain stderr line", logging.ERROR) == logging.ERROR
